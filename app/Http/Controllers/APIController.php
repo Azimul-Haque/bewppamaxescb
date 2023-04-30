8d18be56fdf7bd734d1affd68227584cdba2f82a
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-01 02:27:55 +0600

    Commit Mon 05/01/2023  2:27:55.30 31557

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -97,7 +97,7 @@ class APIController extends Controller
         ]);
     }
 
-    public function addUser(Request $request)
+    public function addOneSignalData(Request $request)
     {
         $this->validate($request,array(
             'uid'         => 'required|max:191|unique:users,uid',

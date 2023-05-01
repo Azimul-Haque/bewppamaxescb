692218c3a0fa85e07a869b48a8b7657614676782
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-02 02:09:42 +0600

    Commit Tue 05/02/2023  2:09:42.29 4169

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -502,6 +502,7 @@ class APIController extends Controller
 
     public function addExamResult(Request $request)
     {
+        return $request->all();
         $this->validate($request,array(
             'mobile'      => 'required',
             'course_id'   => 'required',

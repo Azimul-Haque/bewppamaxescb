f9d33bd3224a2346a6b69a50d67352af082f4e4b
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-02 22:51:37 +0600

    Commit Tue 05/02/2023 22:51:37.21 26990

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -457,7 +457,7 @@ class ExamController extends Controller
                                ->get();
         
         foreach($meritlists as $meritlist) {
-            dd($meritlist->user)
+            dd($meritlist->user);
         }
         dd($meritlists);
     }

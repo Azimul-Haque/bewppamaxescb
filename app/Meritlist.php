1a6c122e19e3777021e4f6f3762e74f027fceca8
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-02 23:28:00 +0600

    Commit Tue 05/02/2023 23:28:00.26 5941

--- app/Meritlist.php
+++ app/Meritlist.php
@@ -10,6 +10,10 @@ class Meritlist extends Model
         return $this->belongsTo('App\Exam');
     }
 
+    public function course(){
+        return $this->belongsTo('App\Course');
+    }
+
     public function user(){
         return $this->belongsTo('App\User');
     }

d3c20171b7def6f0735a3ded8c9d288749d5ebd6
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-03 00:39:28 +0600

    Commit Wed 05/03/2023  0:39:28.30 1442

--- resources/views/dashboard/exams/meritlist.blade.php
+++ resources/views/dashboard/exams/meritlist.blade.php
@@ -39,7 +39,9 @@
                           @foreach($finalmeritlist as $singledata)
                               <tr>
                                   <td>{{ $singledata['coursename'] }}</td>
-                                  <td><a href="{{ route('dashboard.users.single', $singledata['user_id']) }}">{{ $singledata['name'] }}</a></td>
+                                  <td>
+                                    <a href="{{ route('dashboard.users.single', $singledata['user_id']) }}">{{ $singledata['name'] }}</a>
+                                  </td>
                                   <td>{{ $singledata['marks'] }}</td>
                                   <td>{{ $singledata['rank'] }}</td>
                                   <td></td>

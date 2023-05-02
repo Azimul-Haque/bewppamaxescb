13ba868840dedde0579925770fa0a325f79a334b
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-03 02:01:39 +0600

    Commit Wed 05/03/2023  2:01:39.36 9067

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -552,13 +552,12 @@ class APIController extends Controller
             foreach($meritlists as $meritlist) {
                 $meritlist->name = $meritlist->user->name;
             }
-            
+            $meritlists->makeHidden('id');
+            $meritlists->makeHidden('created_at', 'updated_at');
+            $meritlists->makeHidden('updated_at');
+            $meritlists->makeHidden('user_id');
+            $meritlists->makeHidden('user');
             $finalmeritlist = $meritlists->sortByDesc('marks');
-            $finalmeritlist->makeHidden('id');
-            $finalmeritlist->makeHidden('created_at');
-            $finalmeritlist->makeHidden('updated_at');
-            $finalmeritlist->makeHidden('user_id');
-            $finalmeritlist->makeHidden('user');
             // dd($finalmeritlist);
             return response()->json([
                 'success' => true,

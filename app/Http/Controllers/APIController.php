781c64a7047d25dc92aaefa9a4739a9e07cda047
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-03 02:00:51 +0600

    Commit Wed 05/03/2023  2:00:51.28 22442

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -562,7 +562,7 @@ class APIController extends Controller
             // dd($finalmeritlist);
             return response()->json([
                 'success' => true,
-                'meritlists' => $finalmeritlist,
+                'meritlists' => $finalmeritlist->toArray(),
             ]);
         } else {
             return response()->json([

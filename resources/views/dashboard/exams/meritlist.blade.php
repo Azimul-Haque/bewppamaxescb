d81bf001af0a844ad586b736ed043374356bbad4
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-03 00:34:43 +0600

    Commit Wed 05/03/2023  0:34:43.23 21190

--- resources/views/dashboard/exams/meritlist.blade.php
+++ resources/views/dashboard/exams/meritlist.blade.php
@@ -41,7 +41,6 @@
                           @foreach($exams as $exam)
                               <tr>
                                   
-                                  
                               </tr>
                           @endforeach
                           </tbody>

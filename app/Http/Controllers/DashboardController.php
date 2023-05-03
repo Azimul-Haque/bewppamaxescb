1f6ef23f6fa57f7c4181b1b486e626397eb3abb0
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-04 04:19:37 +0600

    Commit Thu 05/04/2023  4:19:37.28 22904

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -104,7 +104,7 @@ class DashboardController extends Controller
             return $user->meritlists->count();
         })->paginate(10);
 
-        $users = Prodcuts::with(array('orders' => function($query) {
+        $users = User::with(array('meritlists' => function($query) {
            $query->select(DB::raw('SELECT * count(status) WHERE status = 2 as count'))
             $query->orderBy('MAX(count)')
          }))->paginate(15);

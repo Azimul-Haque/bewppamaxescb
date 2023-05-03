890bfafe8c4515a4c731de29c6d4412a9f19eaa2
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-04 04:13:40 +0600

    Commit Thu 05/04/2023  4:13:40.28 4591

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -12,7 +12,7 @@
 		<div class="card">
           <div class="card-header">
             <h3 class="card-title">ব্যবহারকারীগণ</h3>
-            <small><a style="margin-left: 5px;">সর্বোচ্চ পরীক্ষার্থী</a></small>
+            <small><a href="" style="margin-left: 5px;">সর্বোচ্চ পরীক্ষার্থী</a></small>
 
             <div class="card-tools">
               <form class="form-inline form-group-lg" action="">

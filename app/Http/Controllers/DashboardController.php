cc811dafb05d92c7de38c02b89fb938afbbe23d4
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-04 04:35:11 +0600

    Commit Thu 05/04/2023  4:35:11.30 801

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -103,7 +103,7 @@ class DashboardController extends Controller
 
         $users = User::with(['meritlists' => function ($query) {
             $query->where('user_id', 'users.id');
-            $query->selectRaw('user_id AS count');
+            $query->selectRaw('COUNT(meritlists.user_id) AS count');
         }])
         // ->orderBy('count', 'desc')
         ->paginate(10);

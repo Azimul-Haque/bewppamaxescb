630849a9032df942108478abd9a1f8880df4eff0
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-06 01:42:33 +0600

    Commit Sat 05/06/2023  1:42:33.30 28349

--- resources/views/dashboard/questions/reported.blade.php
+++ resources/views/dashboard/questions/reported.blade.php
@@ -48,15 +48,15 @@
                           @foreach($reportedquestions as $reportedquestion)
                               <tr>
                                   <td>
-                                      {{ $question->question }}<br/>
-                                      <span class="badge bg-success">{{ $question->topic->name }}</span>
-                                      <span class="badge bg-info">{{ $question->difficulty == 1 ? 'সহজ' : ($question->difficulty == 2 ? 'মধ্যম' : 'কঠিন') }}</span>
-                                      @foreach($question->tags as $tag)
+                                      {{ $reportedquestion->question->question }}<br/>
+                                      <span class="badge bg-success">{{ $reportedquestion->question->topic->name }}</span>
+                                      <span class="badge bg-info">{{ $reportedquestion->question->difficulty == 1 ? 'সহজ' : ($reportedquestion->question->difficulty == 2 ? 'মধ্যম' : 'কঠিন') }}</span>
+                                      @foreach($reportedquestion->question->tags as $tag)
                                         <span class="badge bg-primary">{{ $tag->name }}</span>
                                       @endforeach
                                   </td>
-                                  <td>{{ $question->answer }}</td>
-                                  <td>{{ $question->option1 }}, {{ $question->option2 }}, {{ $question->option3 }}, {{ $question->option4 }}</td>
+                                  <td>{{ $reportedquestion->question->answer }}</td>
+                                  <td>{{ $reportedquestion->question->option1 }}, {{ $reportedquestion->question->option2 }}, {{ $reportedquestion->question->option3 }}, {{ $reportedquestion->question->option4 }}</td>
                                   {{-- <td>
                                       <div class="progress progress-xs">
                                           <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
@@ -64,13 +64,13 @@
                                   </td> --}}
                               
                                   <td>
-                                      <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editQuestionModal{{ $question->id }}">
+                                      <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editQuestionModal{{ $reportedquestion->question->id }}">
                                           <i class="far fa-edit"></i>
                                       </button>
                                       {{-- Edit Question Modal Code --}}
                                       {{-- Edit Question Modal Code --}}
                                       <!-- Modal -->
-                                      <div class="modal fade" id="editQuestionModal{{ $question->id }}" tabindex="-1" role="dialog" aria-labelledby="editQuestionModalLabel" aria-hidden="true" data-backdrop="static">
+                                      <div class="modal fade" id="editQuestionModal{{ $reportedquestion->question->id }}" tabindex="-1" role="dialog" aria-labelledby="editQuestionModalLabel" aria-hidden="true" data-backdrop="static">
                                           <div class="modal-dialog modal-lg" role="document">
                                           <div class="modal-content">
                                               <div class="modal-header bg-success">
@@ -79,27 +79,27 @@
                                                     <span aria-hidden="true">&times;</span>
                                                 </button>
                                               </div>
-                                              <form method="post" action="{{ route('dashboard.questions.update', $question->id) }}" enctype='multipart/form-data'>
+                                              <form method="post" action="{{ route('dashboard.questions.update', $reportedquestion->question->id) }}" enctype='multipart/form-data'>
                                                 <div class="modal-body">
                                                       @csrf
                                                       <div class="input-group mb-3">
-                                                          <input type="text" name="question" class="form-control" value="{{ $question->question }}" placeholder="প্রশ্ন" required>
+                                                          <input type="text" name="question" class="form-control" value="{{ $reportedquestion->question->question }}" placeholder="প্রশ্ন" required>
                                                           <div class="input-group-append">
                                                               <div class="input-group-text"><span class="far fa-question-circle"></span></div>
                                                           </div>
                                                       </div>
                                                       <div class="row">
                                                           <div class="col-md-6">
-                                                              <input type="text" name="option1" value="{{ $question->option1 }}" class="form-control mb-3" placeholder="অপশন ১" required>
+                                                              <input type="text" name="option1" value="{{ $reportedquestion->question->option1 }}" class="form-control mb-3" placeholder="অপশন ১" required>
                                                           </div>
                                                           <div class="col-md-6">
-                                                              <input type="text" name="option2" value="{{ $question->option2 }}" class="form-control mb-3" placeholder="অপশন ২" required>
+                                                              <input type="text" name="option2" value="{{ $reportedquestion->question->option2 }}" class="form-control mb-3" placeholder="অপশন ২" required>
                                                           </div>
                                                           <div class="col-md-6">
-                                                              <input type="text" name="option3" value="{{ $question->option3 }}" class="form-control mb-3" placeholder="অপশন ৩" required>
+                                                              <input type="text" name="option3" value="{{ $reportedquestion->question->option3 }}" class="form-control mb-3" placeholder="অপশন ৩" required>
                                                           </div>
                                                           <div class="col-md-6">
-                                                              <input type="text" name="option4" value="{{ $question->option4 }}" class="form-control mb-3" placeholder="অপশন ৪" required>
+                                                              <input type="text" name="option4" value="{{ $reportedquestion->question->option4 }}" class="form-control mb-3" placeholder="অপশন ৪" required>
                                                           </div>
                                                       </div>
                                                       <div class="row">
@@ -107,10 +107,10 @@
                                                           <div class="input-group mb-3">
                                                               <select name="answer" class="form-control" required>
                                                                   <option selected="" disabled="" value="">সঠিক উত্তর</option>
-                                                                  <option value="1" @if($question->answer == 1) selected @endif>অপশন ১</option>
-                                                                  <option value="2" @if($question->answer == 2) selected @endif>অপশন ২</option>
-                                                                  <option value="3" @if($question->answer == 3) selected @endif>অপশন ৩</option>
-                                                                  <option value="4" @if($question->answer == 4) selected @endif>অপশন ৪</option>
+                                                                  <option value="1" @if($reportedquestion->question->answer == 1) selected @endif>অপশন ১</option>
+                                                                  <option value="2" @if($reportedquestion->question->answer == 2) selected @endif>অপশন ২</option>
+                                                                  <option value="3" @if($reportedquestion->question->answer == 3) selected @endif>অপশন ৩</option>
+                                                                  <option value="4" @if($reportedquestion->question->answer == 4) selected @endif>অপশন ৪</option>
                                                               </select>
                                                               <div class="input-group-append">
                                                                   <div class="input-group-text"><span class="far fa-check-circle"></span></div>
@@ -121,7 +121,7 @@
                                                           <select name="tags_ids[]" class="form-control multiple-select" multiple="multiple" data-placeholder="ট্যাগ">
                                                             @php
                                                               $tag_array = [];
-                                                              foreach($question->tags as $tag) {
+                                                              foreach($reportedquestion->question->tags as $tag) {
                                                                 $tag_array[] = $tag->id;
                                                               } 
                                                             @endphp
@@ -136,9 +136,9 @@
                                                               <div class="input-group mb-3">
                                                                   <select name="difficulty" class="form-control" required>
                                                                       <option selected="" disabled="" value="">ডিফিকাল্টি লেভেল</option>
-                                                                      <option value="1" @if($question->difficulty == 1) selected @endif>সহজ</option>
-                                                                      <option value="2" @if($question->difficulty == 2) selected @endif>মধ্যম</option>
-                                                                      <option value="3" @if($question->difficulty == 3) selected @endif>কঠিন</option>
+                                                                      <option value="1" @if($reportedquestion->question->difficulty == 1) selected @endif>সহজ</option>
+                                                                      <option value="2" @if($reportedquestion->question->difficulty == 2) selected @endif>মধ্যম</option>
+                                                                      <option value="3" @if($reportedquestion->question->difficulty == 3) selected @endif>কঠিন</option>
                                                                   </select>
                                                                   <div class="input-group-append">
                                                                       <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
@@ -150,7 +150,7 @@
                                                                   <select name="topic_id" class="form-control" required>
                                                                       <option selected="" disabled="" value="">টপিক (বিষয়)</option>
                                                                       @foreach ($topics as $topic)
-                                                                          <option value="{{ $topic->id }}" @if($question->topic_id == $topic->id) selected @endif>{{ $topic->name }}</option>
+                                                                          <option value="{{ $topic->id }}" @if($reportedquestion->question->topic_id == $topic->id) selected @endif>{{ $topic->name }}</option>
                                                                       @endforeach
                                                                   </select>
                                                                   <div class="input-group-append">
@@ -161,22 +161,22 @@
                                                           <div class="col-md-6">
                                                               <div class="form-group ">
                                                                   <label for="image">ছবি (প্রয়োজনে)</label>
-                                                                  <input type="file" id="image{{ $question->id }}" name="image" accept="image/*">
+                                                                  <input type="file" id="image{{ $reportedquestion->question->id }}" name="image" accept="image/*">
                                                               </div>
                                                               <center>
                                                                   <?php
-                                                                    if($question->questionimage) {
-                                                                        $currentimage = asset('images/questions/' . $question->questionimage->image);
+                                                                    if($reportedquestion->question->questionimage) {
+                                                                        $currentimage = asset('images/questions/' . $reportedquestion->question->questionimage->image);
                                                                     } else {
                                                                         $currentimage = asset('images/placeholder.png');
                                                                     }
                                                                   ?>
-                                                                  <img src="{{ $currentimage }}" id='img-upload{{ $question->id }}' style="width: 250px; height: auto;" class="img-responsive" />
+                                                                  <img src="{{ $currentimage }}" id='img-upload{{ $reportedquestion->question->id }}' style="width: 250px; height: auto;" class="img-responsive" />
                                                               </center>
                                                           </div>
                                                           <div class="col-md-6">
                                                               <label for="explanation">ব্যাখ্যা (প্রয়োজনে)</label><br/>
-                                                              <textarea class="form-control summernote" name="explanation" id="explanation" placeholder="ব্যাখ্যা" style="width: 100%; height: 220px;">{{ $question->questionexplanation ? $question->questionexplanation->explanation : '' }}</textarea>
+                                                              <textarea class="form-control summernote" name="explanation" id="explanation" placeholder="ব্যাখ্যা" style="width: 100%; height: 220px;">{{ $reportedquestion->question->questionexplanation ? $reportedquestion->question->questionexplanation->explanation : '' }}</textarea>
                                                           </div>
                                                       </div>
                                                 </div>
@@ -209,23 +209,23 @@
           if (input.files && input.files[0]) {
               var reader = new FileReader();
               reader.onload = function (e) {
-                  $('#img-upload{{ $question->id }}').attr('src', e.target.result);
+                  $('#img-upload{{ $reportedquestion->question->id }}').attr('src', e.target.result);
               }
               reader.readAsDataURL(input.files[0]);
           }
       }
-      $("#image{{ $question->id }}").change(function(){
+      $("#image{{ $reportedquestion->question->id }}").change(function(){
           readURL(this);
           var filesize = parseInt((this.files[0].size)/1024);
           if(filesize > 10000) {
-            $("#image{{ $question->id }}").val('');
+            $("#image{{ $reportedquestion->question->id }}").val('');
             // toastr.warning('File size is: '+filesize+' Kb. try uploading less than 300Kb', 'WARNING').css('width', '400px;');
             Toast.fire({
                 icon: 'warning',
                 title: 'File size is: '+filesize+' Kb. try uploading less than 300Kb'
             })
             setTimeout(function() {
-            $("#img-upload{{ $question->id }}").attr('src', '{{ asset('images/placeholder.png') }}');
+            $("#img-upload{{ $reportedquestion->question->id }}").attr('src', '{{ asset('images/placeholder.png') }}');
             }, 1000);
           }
       });

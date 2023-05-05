f27761ad1df5b653b62b338765ba498b7ddc4cb8
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-06 05:00:57 +0600

    Commit Sat 05/06/2023  5:00:57.23 17564

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -161,6 +161,7 @@ class ExamController extends Controller
         $exam->syllabus = nl2br($request->syllabus);
         $exam->save();
 
+        Cache::forget(examcutmark . $id);
         Session::flash('success', 'Exam updated successfully!');
         return redirect()->route('dashboard.exams');
     }

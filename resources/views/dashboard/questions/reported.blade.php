4bad0b5fcb00845b10a9fd0974b0aad2e1691734
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-06 05:57:45 +0600

    Commit Sat 05/06/2023  5:57:45.23 21489

--- resources/views/dashboard/questions/reported.blade.php
+++ resources/views/dashboard/questions/reported.blade.php
@@ -261,7 +261,7 @@
                                                 </div>
                                                 <div class="modal-footer">
                                                   <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                                                  <form method="POST" action="{{ route('dashboard.messages.update', $reportedquestion->id) }}">
+                                                  <form method="POST" action="{{ route('dashboard.questions.reported.delete', $reportedquestion->id) }}">
                                                     @csrf
                                                     <button type="submit" class="btn btn-warning">দাখিল করুন</button>
                                                   </form>

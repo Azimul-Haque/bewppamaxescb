81f1199035c3de1014f8a48294430dc09a7ef7ae
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-06 01:55:51 +0600

    Commit Sat 05/06/2023  1:55:51.22 16610

--- resources/views/dashboard/questions/reported.blade.php
+++ resources/views/dashboard/questions/reported.blade.php
@@ -284,7 +284,7 @@
       $("#search-param").keyup(function(e) {
         if(e.which == 13) {
           if($('#search-param').val() != '') {
-            var urltocall = '{{ route('dashboard.questions') }}' +  '/' + $('#search-param').val();
+            var urltocall = '{{ route('dashboard.reported.questions') }}' +  '/' + $('#search-param').val();
             location.href= urltocall;
           } else {
             $('#search-param').css({ "border": '#FF0000 2px solid'});

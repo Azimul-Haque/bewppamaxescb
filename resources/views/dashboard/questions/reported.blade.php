aa6b3d3876500ad032efa9d117c9a19750258057
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-06 02:05:54 +0600

    Commit Sat 05/06/2023  2:05:54.26 17120

--- resources/views/dashboard/questions/reported.blade.php
+++ resources/views/dashboard/questions/reported.blade.php
@@ -272,8 +272,8 @@
       $(document).on('click', '#search-button', function() {
         if($('#search-param').val() != '') {
           console.log($('#search-param').val().replace(/\\/g, ''));
-          var urltocall = '{{ route('dashboard.questions.reported') }}' +  '/' + $('#search-param').val().replace(/\\/g, '');
-          location.href= urltocall;
+          // var urltocall = '{{ route('dashboard.questions.reported') }}' +  '/' + $('#search-param').val().replace(/\\/g, '');
+          // location.href= urltocall;
         } else {
           $('#search-param').css({ "border": '#FF0000 2px solid'});
           Toast.fire({

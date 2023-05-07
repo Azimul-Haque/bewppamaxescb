2d5aca91badbb1d5f4670c7c95c8950505969dbe
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-07 22:09:57 +0600

    Commit Sun 05/07/2023 22:09:57.28 28319

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -20,7 +20,8 @@
             <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">
-                      <h3 class="card-title">প্রশ্নব্যাংক </h3>(মোটঃ {{ $totalquestions }} টি প্রশ্ন)
+                      <h3 class="card-title">প্রশ্নব্যাংক </h3>
+                      (মোটঃ {{ $totalquestions }} টি প্রশ্ন)
           
                       <div class="card-tools">
                           <form class="form-inline form-group-lg" action="">

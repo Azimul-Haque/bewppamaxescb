4b57dce37c7ec23c598cddbb59ca562c36b37587
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-09 23:31:27 +0600

    Commit Tue 05/09/2023 23:31:27.26 25309

--- database/migrations/2023_05_06_012017_create_reportedquestions_table.php
+++ database/migrations/2023_05_06_012017_create_reportedquestions_table.php
@@ -18,6 +18,7 @@ class CreateReportedquestionsTable extends Migration
             $table->integer('question_id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->string('message')->nullable();
+            $table->integer('status')->default(0);
             $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();

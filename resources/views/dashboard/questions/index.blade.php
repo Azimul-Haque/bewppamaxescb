9c8c925862a0e5ba19382587c5a30533c9ab7d9e
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-18 01:59:09 +0600

    Commit Thu 05/18/2023  1:59:09.32 8564

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -244,6 +244,9 @@
                                       <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteQuestionModal{{ $question->id }}" disabled>
                                           <i class="far fa-trash-alt"></i>
                                       </button>
+                                      <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#notifModal{{ $question->id }}">
+                                    <i class="fas fa-bell"></i>
+                                  </button>
                                   </td>
                                   {{-- Delete Question Modal Code --}}
                                   {{-- Delete Question Modal Code --}}
@@ -273,9 +276,7 @@
                                   {{-- Delete Question Modal Code --}}
                                   {{-- Delete Question Modal Code --}}
 
-                                  <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#notifModal{{ $question->id }}">
-                                    <i class="fas fa-bell"></i>
-                                  </button>
+                                  
                                   {{-- Notif Modal Code --}}
                                   {{-- Notif Modal Code --}}
                                   <!-- Modal -->

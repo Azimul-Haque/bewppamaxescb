13d6942a6087839872380415e57da0a2c015f1fd
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-18 01:57:31 +0600

    Commit Thu 05/18/2023  1:57:31.28 1251

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -313,7 +313,7 @@
                                           </div>
                                           <div class="modal-footer">
                                             <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                                            <button type="submit" class="btn btn-warning">নোটিফিকেশন পাঠান</button>
+                                            <a href="{{ route('dashboard.questions.sendnotification', $question->id) }}" class="btn btn-warning">নোটিফিকেশন পাঠান</a>
                                           </div>
                                       </div>
                                     </div>

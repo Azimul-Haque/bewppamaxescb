407bc6f9d4ddc1e594cbe04692793b5187e0daba
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-18 02:56:09 +0600

    Commit Thu 05/18/2023  2:56:09.24 29873

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -430,7 +430,7 @@ class QuestionController extends Controller
             "উত্তর দেখতে নোটিফিকেশনে ক্লিক করুন",
             ['716ffeb3-f6c2-4a4a-a253-710f339aa863'],
             $url = null, 
-            $data = array("a" => 'answer', "b" => $answertext, 'c' => $question->questionexplanatio ? $question->questionexplanation->explanation : ''),
+            $data = array("a" => 'answer', "b" => $answertext, 'c' => $question->questionexplanation ? $question->questionexplanation->explanation : ''),
             $buttons = null, 
             $schedule = null,
             $headings = $question->question,

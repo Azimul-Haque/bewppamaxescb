fa9269de12be7bfeb66ff643bc017c025f42e03d
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-18 00:12:45 +0600

    Commit Thu 05/18/2023  0:12:45.29 1071

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -509,7 +509,7 @@ class APIController extends Controller
         {
             $material = Cache::remember('singlelecturematerial', 7 * 24 * 60 * 60, function () {
                 $material = Material::where('id', $id)
-                                     ->select('id', 'type', 'title', 'author', 'author_desc', 'content'. 'count')
+                                     ->select('id', 'type', 'title', 'author', 'author_desc', 'content', 'url', 'count')
                                      ->first();
 
                 // foreach($materials as $material) {

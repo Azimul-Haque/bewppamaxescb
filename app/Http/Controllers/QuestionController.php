b7ca6d1978e7383ef44483793af389f6db64ecbc
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-18 02:03:20 +0600

    Commit Thu 05/18/2023  2:03:20.29 10026

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -423,6 +423,9 @@ class QuestionController extends Controller
         //     $headings = $charioteer->question
         // );
 
+        $answertext = $question['option' . $question->answer];
+        dd($answertext);
+
         OneSignal::sendNotificationToUser(
             "উত্তর দেখতে নোটিফিকেশনে ক্লিক করুন",
             ['716ffeb3-f6c2-4a4a-a253-710f339aa863'],

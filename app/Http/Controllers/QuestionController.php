f33f7b6c84b7d8c488c2a8dae38b015558839787
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-18 02:15:26 +0600

    Commit Thu 05/18/2023  2:15:26.27 30614

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -430,7 +430,7 @@ class QuestionController extends Controller
             "উত্তর দেখতে নোটিফিকেশনে ক্লিক করুন",
             ['716ffeb3-f6c2-4a4a-a253-710f339aa863'],
             $url = null, 
-            $data = ["type" => 'answer', "answer" => $answertext],
+            $data = ['answer', $answertext],
             $buttons = null, 
             $schedule = null,
             $headings = $question->question,

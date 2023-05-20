e9beac4939db16717237daffa3998bbe8cc65a51
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-20 16:02:21 +0600

    Commit Sat 05/20/2023 16:02:21.29 23745

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -95,8 +95,7 @@
                                                 <div class="modal-body">
                                                       @csrf
                                                       <div class="input-group mb-3">
-                                                          <div id="questionsummernote" style="width100%"></div>
-                                                          <textarea id="question" name="question" style="display: none;"></textarea>
+                                                          <textarea id="summernote{{ $material->id }}" name="question">{!! $question->question !!}</textarea>
                                                           {{-- <input type="text" name="question" class="form-control" value="{{ $question->question }}" placeholder="প্রশ্ন" required> --}}
                                                           <div class="input-group-append">
                                                               <div class="input-group-text"><span class="far fa-question-circle"></span></div>

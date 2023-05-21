0fc2417db047338fa7b19cac72d3b0c0ac8ced87
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-22 01:53:14 +0600

    Commit Mon 05/22/2023  1:53:14.24 4032

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -429,7 +429,6 @@ class QuestionController extends Controller
         //     $headings = $question->question,
         // );
 
-        $strippedquestion = strip_tags($question->question);
         OneSignal::sendNotificationToUser(
             "উত্তর দেখতে নোটিফিকেশনে ক্লিক করুন",
             ['7198ee60-23a0-446e-ab00-2599f4add299'], // 716ffeb3-f6c2-4a4a-a253-710f339aa863
@@ -437,7 +436,7 @@ class QuestionController extends Controller
             $data = array("a" => 'answer', "b" => $answertext, 'c' => $question->questionexplanation ? $question->questionexplanation->explanation : '', "d" => $question->question),
             $buttons = null, 
             $schedule = null,
-            $headings = $strippedquestion != '' $strippedquestion : 'ছবিতে প্রশ্নটি দেখুন ও উত্তর করুন!',
+            $headings = strip_tags($question->question) != '' strip_tags($question->question) : 'ছবিতে প্রশ্নটি দেখুন ও উত্তর করুন!',
         );
 
         Session::flash('success', 'Question sent in Notification successfully!');

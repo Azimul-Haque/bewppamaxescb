24706dc4b0d66566197bd3408595638e7a30803c
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-25 02:51:28 +0600

    Commit Thu 05/25/2023  2:51:28.23 20613

--- app/Reportedquestion.php
+++ app/Reportedquestion.php
@@ -5,9 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Reportedquestion extends Model
-{
-    public $timestamps = true;
-    
+{    
     public function question(){
         return $this->belongsTo('App\Question');
     }

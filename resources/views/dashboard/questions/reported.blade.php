56d412643b4641ae75f9488116ad828be44acb7c
Azimul-Haque <orbachinujbuk@gmail.com>
2023-05-25 02:41:20 +0600

    Commit Thu 05/25/2023  2:41:20.25 29061

--- resources/views/dashboard/questions/reported.blade.php
+++ resources/views/dashboard/questions/reported.blade.php
@@ -74,7 +74,10 @@
                                     <a href="{{ route('dashboard.users.single', $reportedquestion->user->id) }}">{{ $reportedquestion->user->name }}</a>
                                   </td>
                                   @if(Auth::user()->role == 'admin')
-                                    <td><small>{{ date('d M, Y h:i a', strtotime($reportedquestion->updated_at)) }}</small></td>
+                                    <td>
+                                      <small>{{ date('d M, Y h:i a', strtotime($reportedquestion->updated_at)) }}</small>
+                                      <small>{{ date('d M, Y h:i a', strtotime($reportedquestion->created_at)) }}</small>
+                                    </td>
                                   @endif
                                   <td>{{ $reportedquestion->message }}</td>                     
                                   <td>

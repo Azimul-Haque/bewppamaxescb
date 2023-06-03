13b4b1ca0f8f8f51f624f65f435f95b4fb54611d
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-03 21:43:13 +0600

    Commit Sat 06/03/2023 21:43:13.31 21736

--- resources/views/dashboard/exams/index.blade.php
+++ resources/views/dashboard/exams/index.blade.php
@@ -532,6 +532,16 @@
                           <div class="input-group-text"><span class="far fa-bookmark"></span></div>
                       </div>
                   </div>
+                  <div class="input-group mb-3">
+                      <input type="text"
+                             name="name"
+                             class="form-control"
+                             value="{{ old('name') }}"
+                             placeholder="নাম" required>
+                      <div class="input-group-append">
+                          <div class="input-group-text"><span class="far fa-bookmark"></span></div>
+                      </div>
+                  </div>
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>

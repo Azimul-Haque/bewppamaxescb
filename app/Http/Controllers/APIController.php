a31b18315cae68d58d29c9dbed6ad7036b4669be
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-05 00:37:54 +0600

    Commit Mon 06/05/2023  0:37:54.32 27526

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -669,13 +669,13 @@ class APIController extends Controller
             // UPORER TA THEKE ID ASBE 6, SETA HOCCHE QB ER COURSE ID
 
 
-            $courseexams = Cache::remember('questionbank'.$getexamcategory, 10 * 24 * 60 * 60, function () use ($getexamcategory) {
-                $courseexams = Courseexam::select('course_id', 'exam_id')
+            $allcatcourseexams = Cache::remember('questionbank'.$getexamcategory, 10 * 24 * 60 * 60, function () use ($getexamcategory) {
+                $allcatcourseexams = Courseexam::select('course_id', 'exam_id')
                                          ->where('course_id', 6) // MANUALLY BOSAY DILAM
                                          ->orderBy('exam_id', 'desc')
                                          ->get();
 
-                foreach($courseexams as $courseexam) {
+                foreach($allcatcourseexams as $courseexam) {
                     if($courseexam->exam->examcategory_id == $getexamcategory) {
                         $courseexam->name = $courseexam->exam->name;
                         $courseexam->start = $courseexam->exam->available_from;

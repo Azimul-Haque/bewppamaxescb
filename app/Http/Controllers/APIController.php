b38cdd4f6b51226eedd87dcf9a397b32364e6720
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-05 00:47:59 +0600

    Commit Mon 06/05/2023  0:47:59.32 26751

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -668,7 +668,7 @@ class APIController extends Controller
             //                  ->first();
             // UPORER TA THEKE ID ASBE 6, SETA HOCCHE QB ER COURSE ID
 
-            $courseexams = Cache::remember('questionbank'.$getexamcategory, 10 * 24 * 60 * 60, function () use ($getexamcategory) {
+            $courseexamsreturn = Cache::remember('questionbank'.$getexamcategory, 10 * 24 * 60 * 60, function () use ($getexamcategory) {
                 $courseexams = Courseexam::select('course_id', 'exam_id')
                                          ->where('course_id', 6) // MANUALLY BOSAY DILAM
                                          ->orderBy('exam_id', 'desc')

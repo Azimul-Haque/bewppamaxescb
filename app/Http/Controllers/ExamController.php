968075403e72cc9a7f428d664b95d268ecb8bcd2
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-08 00:12:59 +0600

    Commit Thu 06/08/2023  0:12:59.30 9744

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -153,9 +153,11 @@ class ExamController extends Controller
         $exam->price_type = 1; // paid
         $exam->cutmark = $request->cutmark;
         if($request->alltimeavailability) {
-            
+
+        } else {
+            $exam->available_from = Carbon::parse($request->available_from);
         }
-        $exam->available_from = Carbon::parse($request->available_from);
+        
         $exam->available_to = Carbon::parse($request->available_to);
         $exam->syllabus = nl2br($request->syllabus);
         $exam->save();

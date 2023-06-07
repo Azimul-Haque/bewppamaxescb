5d5a3f0c6980f787e589f6ba510b00db0a5bebb5
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-08 00:16:22 +0600

    Commit Thu 06/08/2023  0:16:22.25 3662

--- resources/views/dashboard/exams/index.blade.php
+++ resources/views/dashboard/exams/index.blade.php
@@ -218,7 +218,7 @@
                                                         <div class="col-md-12 mb-3">
                                                             <div class="form-check-inline">
                                                               <label class="form-check-label">
-                                                                <input type="checkbox" class="form-check-input" name="alltimeavailability" value="1">সব সময় এভেইলেবল
+                                                                <input type="checkbox" class="form-check-input" name="alltimeavailability" value="1" @if($exam->alltimeavailability == 1) checked @endif>সব সময় এভেইলেবল
                                                               </label>
                                                             </div>
                                                         </div>

2fdc1da97dcd60ac8b5f1ee004c5d8391d00ae3c
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-08 00:12:51 +0600

    Commit Thu 06/08/2023  0:12:51.30 31938

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -152,6 +152,9 @@ class ExamController extends Controller
         $exam->negativepercentage = $request->negativepercentage;
         $exam->price_type = 1; // paid
         $exam->cutmark = $request->cutmark;
+        if($request->alltimeavailability) {
+            
+        }
         $exam->available_from = Carbon::parse($request->available_from);
         $exam->available_to = Carbon::parse($request->available_to);
         $exam->syllabus = nl2br($request->syllabus);

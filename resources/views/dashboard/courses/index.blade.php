ee0dcadda6e6a7fc8cb5662b2494a77c4b1b230b
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-10 21:02:36 +0600

    Commit Sat 06/10/2023 21:02:36.21 20143

--- resources/views/dashboard/courses/index.blade.php
+++ resources/views/dashboard/courses/index.blade.php
@@ -148,7 +148,7 @@
                                                   <div class="modal-body">
                                                     @csrf
                                                     <div class="input-group mb-3">
-                                                        <input type="text" name="available_from" id="available_from{{ $exam->id }}" value="{{ date('F d, Y', strtotime($exam->available_from)) }}" class="form-control" autocomplete="off" placeholder="চালু হবে" required>
+                                                        <input type="text" name="available_from" id="available_from" class="form-control" autocomplete="off" placeholder="চালু হবে" required>
                                                         <div class="input-group-append">
                                                             <div class="input-group-text"><span class="fas fa-calendar-check"></span></div>
                                                         </div>

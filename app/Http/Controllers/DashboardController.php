bca344ad93ac28f2113d0de4aebc44b0d7d6c9b4
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-14 01:41:12 +0600

    Commit Wed 06/14/2023  1:41:12.27 2561

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -130,7 +130,7 @@ class DashboardController extends Controller
         dd($paidusersids);
         $userscount = User::where('package_expiry_date', '<', Carbon::now())->count();
         $users = User::where('package_expiry_date', '<', Carbon::now())
-                     ->whereIn('id', $paidusersids)
+                     ->whereIn('id', [1, 12])
                      ->orderBy('package_expiry_date', 'asc')
                      ->paginate(10);
         

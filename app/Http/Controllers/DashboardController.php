75554c62a78d65691ebc366f8f1e7c12b35d1d55
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-14 01:38:07 +0600

    Commit Wed 06/14/2023  1:38:07.33 23310

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -127,7 +127,7 @@ class DashboardController extends Controller
     public function getExpiredUsers()
     {
         $paidusersids = DB::table('payments')->select('user_id')->groupBy('user_id')->get()->toArray();
-        $arraytest = array_values$paidusersids);
+        $arraytest = array_values($paidusersids);
         dd($arraytest);
         $userscount = User::where('package_expiry_date', '<', Carbon::now())->count();
         $users = User::withCount('payments')

5d9b358765b27bfc6d5bbdc99501612f66fced34
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-14 01:24:06 +0600

    Commit Wed 06/14/2023  1:24:06.26 21028

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -134,7 +134,7 @@ class DashboardController extends Controller
         //     return $value->payments_count > 0;
         // });
 
-        dd($users->count());
+        dd($users);
         return view('dashboard.users.index')
                     ->withUsers($users)
                     ->withUserscount($userscount);

eab3ac80d84628fe2604e8770d90b47fa8d469e2
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-14 02:32:13 +0600

    Commit Wed 06/14/2023  2:32:13.22 10235

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -168,7 +168,7 @@ class DashboardController extends Controller
             $smsdata = array_values($smsdata);
             $smsjsondata = json_encode($smsdata);
             $data= array(
-                'data'=>"$smsjsondata",
+                'messages'=>"$smsjsondata",
                 'username'=>config('sms.username'),
                 'password'=>config('sms.password'),
             );

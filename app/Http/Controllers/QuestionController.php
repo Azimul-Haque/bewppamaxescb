bb6ebf83f0c2671af2a53cf009f3f8de9322e824
Azimul-Haque <orbachinujbuk@gmail.com>
2023-06-14 22:16:34 +0600

    Commit Wed 06/14/2023 22:16:34.33 21380

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -86,6 +86,7 @@ class QuestionController extends Controller
 
     public function getQuestionsTopicBased($id)
     {
+        ini_set('memory_limit', '-1');
         if(!(Auth::user()->role == 'admin' || Auth::user()->role == 'manager')) {
             abort(403, 'Access Denied');
         }

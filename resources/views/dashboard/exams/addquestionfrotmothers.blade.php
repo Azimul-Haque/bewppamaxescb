5e94f15ab30b9dd826b661b8ffb429da32ebfd31
Azimul-Haque <orbachinujbuk@gmail.com>
2023-07-22 15:03:52 +0600

    Commit Sat 07/22/2023 15:03:52.43 6785

--- resources/views/dashboard/exams/addquestionfrotmothers.blade.php
+++ resources/views/dashboard/exams/addquestionfrotmothers.blade.php
@@ -84,7 +84,7 @@
       console.log(selectarray[1]);
       var appendhtml = '<tr id="tablerow'+selectarray[1]+'">';
       appendhtml += '<td width="70%">' + selectarray[0] + '</td>';
-      appendhtml += '<td width="20%"><input class="form-control" name="questionamount'+selectarray[1]+'" placeholder="প্রশ্নের সংখ্যা"></td>';
+      appendhtml += '<td width="20%"><input type="number" class="form-control" name="questionamount'+selectarray[1]+'" placeholder="প্রশ্নের সংখ্যা"></td>';
       appendhtml += '<td align="right"><button type="button" class="btn btn-danger btn-sm" onclick="removeRow(tablerow'+selectarray[1]+')"><i class="far fa-trash-alt"></i></button</td>';
       appendhtml += '</tr>';
       appendhtml += '<input type="hidden" name="examid" value="'+selectarray[1]+'">';

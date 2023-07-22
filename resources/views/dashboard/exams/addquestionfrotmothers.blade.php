6804c0d0a9c7f7aa7fde3f412cecdd7a65844eea
Azimul-Haque <orbachinujbuk@gmail.com>
2023-07-22 17:12:19 +0600

    Commit Sat 07/22/2023 17:12:19.37 9218

--- resources/views/dashboard/exams/addquestionfrotmothers.blade.php
+++ resources/views/dashboard/exams/addquestionfrotmothers.blade.php
@@ -38,6 +38,7 @@
                       <form method="post" action="{{ route('dashboard.exams.question.from.others.store', $exam->id) }}">
                         @csrf
                         <table id="selectedquestionlist"></table><br/>
+                        <input type="hidden" name="examid" value="{{ $exam->id }}">
                         <button type="submit" class="btn btn-success" style="float: right;">দাখিল করুন</button>
                       </form>
                     </div>

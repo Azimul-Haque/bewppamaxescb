1b09e16f46c45de9c645f6674ef26e3a332fb467
Azimul-Haque <orbachinujbuk@gmail.com>
2023-07-22 14:38:17 +0600

    Commit Sat 07/22/2023 14:38:17.49 19682

--- resources/views/dashboard/exams/addquestionfrotmothers.blade.php
+++ resources/views/dashboard/exams/addquestionfrotmothers.blade.php
@@ -82,10 +82,10 @@
       console.log(selectarray[0]);
       console.log(selectarray[1]);
       var appendhtml = '<table><tr>';
-      var appendhtml = '<td>' + selectarray[0] + '</td>';
-      var appendhtml = '<td><input class="form-control" name=""></td>';
-      var appendhtml = '<td><button>X</button</td>';
-      var appendhtml = '</tr>/<table>';
+      appendhtml = '<td>' + selectarray[0] + '</td>';
+      appendhtml = '<td><input class="form-control" name=""></td>';
+      appendhtml = '<td><button>X</button</td>';
+      appendhtml = '</tr>/<table>';
       $('#selectedquestionlist').append(appendhtml);
     });
 </script>

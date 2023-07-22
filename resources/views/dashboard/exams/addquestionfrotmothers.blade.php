529e59f195ebf5781d8bccf375fb3697c721757f
Azimul-Haque <orbachinujbuk@gmail.com>
2023-07-22 14:59:07 +0600

    Commit Sat 07/22/2023 14:59:07.41 8141

--- resources/views/dashboard/exams/addquestionfrotmothers.blade.php
+++ resources/views/dashboard/exams/addquestionfrotmothers.blade.php
@@ -87,7 +87,7 @@
       appendhtml += '<td width="20%"><input class="form-control" name="questionamount'+selectarray[1]+'" placeholder="প্রশ্নের সংখ্যা"></td>';
       appendhtml += '<td align="right"><button type="button" class="btn btn-success btn-sm" onclick="removeRow(tablerow'+selectarray[1]+')">X</button</td>';
       appendhtml += '</tr>';
-      appendhtml += '<input type="hidden">';
+      appendhtml += '<input type="hidden" name="examid" value="'+selectarray[1]+'">';
       $('#selectedquestionlist').append(appendhtml);
     });
     function removeRow(rowid) {

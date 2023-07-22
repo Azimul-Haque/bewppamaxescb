c8c8979ab736331d5b64bb2ffe1fd78bee6e1e5a
Azimul-Haque <orbachinujbuk@gmail.com>
2023-07-22 14:06:58 +0600

    Commit Sat 07/22/2023 14:06:58.36 1768

--- resources/views/dashboard/exams/addquestionfrotmothers.blade.php
+++ resources/views/dashboard/exams/addquestionfrotmothers.blade.php
@@ -24,7 +24,7 @@
                     <div class="card-header">
                       <h3 class="card-title">প্রশ্নপত্র থেকে এড করুন</h3>
                       <div class="card-tools">
-                          <form>
+                          <form method="post" action="{{ route('dashboard.exams.question.store') }}">
                               <select name="tags_ids[]" class="form-control multiple-select" multiple="multiple" data-placeholder="ট্যাগ">
                                 @php
                                   $tag_array = [];

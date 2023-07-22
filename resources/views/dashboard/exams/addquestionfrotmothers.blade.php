4b3baa598984375980da4a1cade9aa88404186a5
Azimul-Haque <orbachinujbuk@gmail.com>
2023-07-22 14:50:36 +0600

    Commit Sat 07/22/2023 14:50:36.40 15348

--- resources/views/dashboard/exams/addquestionfrotmothers.blade.php
+++ resources/views/dashboard/exams/addquestionfrotmothers.blade.php
@@ -29,7 +29,7 @@
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
-                      <select class="form-control select2" id="questionsetselect" data-placeholder="পরীক্ষার নাম">
+                      <select class="form-control select2" data-placeholder="পরীক্ষার নাম">
                         <option value="" selected disabled>পরীক্ষার নাম</option>
                         @foreach ($exams as $exam)
                             <option value="{{ $exam->name }},{{ $exam->id }}">{{ $exam->name }}</option>

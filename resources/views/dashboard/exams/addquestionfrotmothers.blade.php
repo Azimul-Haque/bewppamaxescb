75ac3785eab953022b6c9d9669f86d67c8538871
Azimul-Haque <orbachinujbuk@gmail.com>
2023-07-22 17:32:51 +0600

    Commit Sat 07/22/2023 17:32:51.32 6834

--- resources/views/dashboard/exams/addquestionfrotmothers.blade.php
+++ resources/views/dashboard/exams/addquestionfrotmothers.blade.php
@@ -98,7 +98,7 @@
     function removeRow(rowid) {
       // console.log(rowid);
       $('#tablerow' + rowid).remove();
-      otherexamids.splice( $.inArray(rowid,otherexamids), 1);
+      otherexamids.splice($.inArray(rowid, otherexamids), 1);
       $('#otherexamids').val(otherexamids);
     }
 </script>

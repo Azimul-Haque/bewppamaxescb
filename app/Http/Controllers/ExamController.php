9ad60612949e0009ef82c02280104a1e51911f2f
Azimul-Haque <orbachinujbuk@gmail.com>
2023-07-22 18:12:29 +0600

    Commit Sat 07/22/2023 18:12:29.40 17176

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -320,7 +320,7 @@ class ExamController extends Controller
         $otherexamids = explode(',', $request->otherexamids);
 
         foreach($otherexamids as $examid) {
-            $nameofthevariable = 'questionamount' . $examid
+            $nameofthevariable = 'questionamount' . $examid;
             dd($request->questionamount . $examid);
             // $selectedexam = Examquestion::where('exam_id', $examid)
         }

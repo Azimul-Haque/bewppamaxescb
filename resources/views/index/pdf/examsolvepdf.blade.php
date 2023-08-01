7b2098c808037fcb91adf5f87343803836590efe
Azimul-Haque <orbachinujbuk@gmail.com>
2023-08-02 01:58:18 +0600

    Commit Wed 08/02/2023  1:58:18.34 31523

--- resources/views/index/pdf/examsolvepdf.blade.php
+++ resources/views/index/pdf/examsolvepdf.blade.php
@@ -26,6 +26,7 @@
     background-size: cover;              
     background-repeat: no-repeat;
     background-position: center center;
+    margin: 100px 25px;
   }
   .page-header,
   .page-header-space {

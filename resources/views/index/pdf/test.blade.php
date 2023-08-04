fd27a396cd18a2be1e07990facbd43c39836e2ac
Azimul-Haque <orbachinujbuk@gmail.com>
2023-08-05 04:07:53 +0600

    Commit Sat 08/05/2023  4:07:53.25 30878

--- resources/views/index/pdf/test.blade.php
+++ resources/views/index/pdf/test.blade.php
@@ -48,7 +48,7 @@
     		</table><br/>
     		<div style="background: #E8FFF3; padding: 10px;">
     			@if($question->question->questionexplanation)
-    				<b>ব্যাখ্যা:</b> <span style="padding-bottom: 10px;">{{ $question->question->questionexplanation->explanation }}</span><br/>
+    				<p style="padding-bottom: 10px;"><b>ব্যাখ্যা:</b> {{ $question->question->questionexplanation->explanation }}</p><br/>
     			@endif
     			@if($question->question->questionimage)
     			<img class="img-responsive" src="{{ asset('/images/questions/' . $question->question->questionimage->image) }}"><br/>

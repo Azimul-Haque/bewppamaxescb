3a917763a16ab689af8b26dd4e93159e7e271906
Azimul-Haque <orbachinujbuk@gmail.com>
2023-08-05 03:26:47 +0600

    Commit Sat 08/05/2023  3:26:47.31 32340

--- resources/views/index/pdf/test.blade.php
+++ resources/views/index/pdf/test.blade.php
@@ -13,7 +13,7 @@
 
 <div class="jumbotron text-center">
   <h1>{{ $exam->name }}</h1>
-  <p>পূর্ণমান - {{ bangla($exam->examquestions->count() * $exam->qsweight) }}, সময় - {{ bangla($exam->duration) }} মিনিট</p> 
+  <p>পূর্ণমান - {{ bangla($exam->examquestions->count() * $exam->qsweight) }}, কাটমার্ক - {{ $exam->cutmarks }} সময় - {{ bangla($exam->duration) }} মিনিট</p> 
 </div>
   
 <div class="container">

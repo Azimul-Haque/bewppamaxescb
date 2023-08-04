7dba0d3ba94a46f4e8fe0622aa3330d5d5783ca7
Azimul-Haque <orbachinujbuk@gmail.com>
2023-08-05 03:43:49 +0600

    Commit Sat 08/05/2023  3:43:49.28 24653

--- resources/views/index/pdf/test.blade.php
+++ resources/views/index/pdf/test.blade.php
@@ -36,7 +36,7 @@
     		</table><br/>
     		<p><b>ব্যাখ্যা:</b> {{ $question->question->questionexplanation ? $question->question->questionexplanation->explanation : '' }}</p>
     		@if($question->question->questionimage)
-    		<img class="img-responsive" src="{{ asset('/images/questions/' . $question->question->questionimages->image) }}" >
+    		<img class="img-responsive" src="{{ asset('/images/questions/' . $question->question->questionimages->image) }}">
     		@endif
     		<br/>
 

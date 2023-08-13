91615aafe44d9886b92d326caee028b061f94909
Azimul-Haque <orbachinujbuk@gmail.com>
2023-08-13 22:55:30 +0600

    Commit Sun 08/13/2023 22:55:30.30 15221

--- resources/views/dashboard/questions/changetopic.blade.php
+++ resources/views/dashboard/questions/changetopic.blade.php
@@ -120,9 +120,7 @@
                       <h3 class="card-title">টপিকসমূহ</h3>
           
                       <div class="card-tools">
-                          {{-- <button type="button" class="btn btn-warning btn-sm"  data-toggle="modal" data-target="#addTopicModal">
-                              <i class="fas fa-plus-circle"></i> নতুন টপিক
-                          </button> --}}
+                         
                       </div>
                     </div>
                     <!-- /.card-header -->

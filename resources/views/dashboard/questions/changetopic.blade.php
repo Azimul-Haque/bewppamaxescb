9feceb7dc2615d832954a93d80415fe7af177e34
Azimul-Haque <orbachinujbuk@gmail.com>
2023-08-13 22:55:53 +0600

    Commit Sun 08/13/2023 22:55:53.27 25748

--- resources/views/dashboard/questions/changetopic.blade.php
+++ resources/views/dashboard/questions/changetopic.blade.php
@@ -95,14 +95,7 @@
                                     @else
                                         {{ $question->option4 }}
                                     @endif
-                                  </td>
-                                  {{-- <td>
-                                      <div class="progress progress-xs">
-                                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                                      </div>
-                                  </td> --}}
-                              
-                                  
+                                  </td>                              
                                       
                                 
                               </tr>

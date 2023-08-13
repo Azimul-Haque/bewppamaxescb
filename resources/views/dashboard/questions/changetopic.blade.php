dde8113311aa3b4530b335d3ed72a294ecf71fd5
Azimul-Haque <orbachinujbuk@gmail.com>
2023-08-14 00:42:12 +0600

    Commit Mon 08/14/2023  0:42:12.28 5947

--- resources/views/dashboard/questions/changetopic.blade.php
+++ resources/views/dashboard/questions/changetopic.blade.php
@@ -76,8 +76,8 @@
                                     @if($question->answer == 2)
                                         <big><b>{{ $question->option2 }}</b></big>, 
                                     @else
-                                        {{ $question->option2 }},<br/> 
-                                    @endif
+                                        {{ $question->option2 }},
+                                    @endif<br/> 
                                     @if($question->answer == 3)
                                         <big><b>{{ $question->option3 }}</b></big>, 
                                     @else

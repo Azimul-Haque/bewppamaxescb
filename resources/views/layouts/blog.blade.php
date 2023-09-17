64a20af930c60b04e059beb5371278605fa2afb7
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-17 22:28:55 +0600

    Commit Sun 09/17/2023 22:28:55.31 13685

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -11,6 +11,12 @@
                 @include('partials._blog_sidebar')
             </div>
         </div>
+        <div class="row">
+            <div class="col-md-8 col-sm-8">@yield('content-s')</div>
+            <div class="col-md-3 col-sm-4 col-md-offset-1 sidebar xs-margin-top-ten">
+                @include('partials._blog_sidebar')
+            </div>
+        </div>
     </div>
 @endsection
 

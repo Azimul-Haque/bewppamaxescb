f5a6202e553b1381ac73d722a80216569ce7af09
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-17 22:27:04 +0600

    Commit Sun 09/17/2023 22:27:04.30 8031

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -6,7 +6,7 @@
 @endsection
 
 @section('content-s')
-    <section style="padding-top: 150px; padding-bottom: 50px; background-color: var(--light-3);"></section>
+    <section style="padding-top: 150px; padding-bottom: 50px; background-color: var(--light-4);"></section>
 @endsection
 
 @section('third_party_scripts-s')

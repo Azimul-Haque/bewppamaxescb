bea6c05049a1f7092aa9cf8dbe8c98d5feadb1ee
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-17 23:30:42 +0600

    Commit Sun 09/17/2023 23:30:42.32 20507

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -33,7 +33,7 @@
                     <span id="comment_count{{ $blog->id }}"></span>
                      comment(s)</a>
                 </div>
-                <a class="btn btn-small" href="{{ route('blog.single', $blog->slug) }}">Continue Reading</a>
+                <a class="btn btn-default" href="{{ route('blog.single', $blog->slug) }}">Continue Reading</a>
             </div>
         </div>
         <script type="text/javascript" src="{{ asset('vendor/hcode/js/jquery.min.js') }}"></script>

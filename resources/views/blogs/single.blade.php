e11f70e5b802cfe8b262cbfd1437d8cec67e39e4
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-17 23:53:31 +0600

    Commit Sun 09/17/2023 23:53:31.31 30463

--- resources/views/blogs/single.blade.php
+++ resources/views/blogs/single.blade.php
@@ -2,7 +2,44 @@
 @section('title-secondary') ব্লগ-Blog | BCS Exam - বিসিএস পরীক্ষা | বিসিএস-সহ সরকারি চাকরির পরীক্ষার প্রস্তুতির জন্য সেরা অনলাইন প্ল্যাটফর্ম @endsection
 
 @section('third_party_stylesheets-s')
-    
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/stylesheet.css') }}">
+    @if($blog->featured_image != null)
+        <meta property="og:image" content="{{ asset('images/blogs/'.$blog->featured_image) }}" />
+    @else
+        <meta property="og:image" content="{{ asset('images/abc.png') }}" />
+    @endif
+
+    <meta property="og:title" content="{{ $blog->title }} | {{ $blog->user->name }}"/>
+    <meta name="description" property="og:description" content="{{ substr(strip_tags($blog->body), 0, 200) }}" />
+    <meta property="og:type" content="article"/>
+    <meta property="og:url" content="{{ Request::url() }}" />
+    <meta property="og:site_name" content="TenX">
+    <meta property="og:locale" content="en_US">
+    <meta property="fb:admins" content="100001596964477">
+    <meta property="fb:app_id" content="163879201229487">
+    <meta property="og:type" content="article">
+    <!-- Open Graph - Article -->
+    <meta name="article:section" content="{{ $blog->category->name }}">
+    <meta name="article:published_time" content="{{ $blog->created_at}}">
+    <meta name="article:author" content="{{ Request::url('blogger/profile/'.$blog->user->unique_key) }}">
+    <meta name="article:tag" content="{{ $blog->category->name }}">
+    <meta name="article:modified_time" content="{{ $blog->updated_at}}">
+
+    <style type="text/css">
+        .youtibecontainer {
+            position: relative;
+            width: 100%;
+            height: 0;
+            padding-bottom: 56.25%;
+        }
+        .youtubeiframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
 @endsection
 
 @section('content-s')

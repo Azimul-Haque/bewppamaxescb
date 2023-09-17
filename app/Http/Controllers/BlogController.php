1700b9d396d37e46e29e822989c8ffc39595deb7
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-17 22:42:32 +0600

    Commit Sun 09/17/2023 22:42:32.32 1808

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -33,6 +33,7 @@ class BlogController extends Controller {
                         ->get();
                         //dd($archives);
         $blogs = Blog::orderBy('id', 'desc')->paginate(7);
+        dd($blogs);
         return view('blogs.index')
                   ->withBlogs($blogs)
                   ->withCategories($categories)

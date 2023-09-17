ff06bcb14d5ef7d2e506e5a219e1b042abc3134a
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-18 00:09:07 +0600

    Commit Mon 09/18/2023  0:09:07.26 25152

--- resources/views/blogs/single.blade.php
+++ resources/views/blogs/single.blade.php
@@ -177,7 +177,7 @@
 @endsection
 
 @section('third_party_scripts-s')
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script type="text/javascript">
         $(document).ready(function(){
             $('.note-video-clip').each(function() {

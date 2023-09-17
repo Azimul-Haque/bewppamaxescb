0be7ffe84e17be5cd55cda7aed3789bb1118d6ed
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-17 22:29:10 +0600

    Commit Sun 09/17/2023 22:29:10.24 9979

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -6,7 +6,7 @@
 @endsection
 
 @section('content-s')
-    <section style="padding-top: 150px; padding-bottom: 50px; background-color: #EEEEEE;">
+    <section style="padding-top: 150px; padding-bottom: 50px; background-color: #FFFFFF;">
         Test<br/>
         Test<br/>
         Test<br/>

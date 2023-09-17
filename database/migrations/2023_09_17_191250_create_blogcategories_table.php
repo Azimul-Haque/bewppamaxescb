c88f6924bb6a1ad11ed0c247c8f3e60c90cc9ef2
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-17 19:13:22 +0600

    Commit Sun 09/17/2023 19:13:22.29 19718

--- database/migrations/2023_09_17_191250_create_blogcategories_table.php
+++ database/migrations/2023_09_17_191250_create_blogcategories_table.php
@@ -14,8 +14,8 @@ class CreateBlogcategoriesTable extends Migration
     public function up()
     {
         Schema::create('blogcategories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id');
+            $table->string('name')->unique();
         });
     }
 

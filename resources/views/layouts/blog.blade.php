6eea8e7c8cbc7acc90ae1bbfd2674e3a66098c19
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-17 22:38:04 +0600

    Commit Sun 09/17/2023 22:38:04.27 26386

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -6,20 +6,21 @@
 @endsection
 
 @section('content')
-    <section style="background-color: #F4F4F4;"></section>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-sm-8">@yield('content-s')</div>
-            <div class="col-md-3 col-sm-4 col-md-offset-1 sidebar" style="padding-top: 150px; padding-bottom: 50px;">
-                @include('partials._blog_sidebar')
+    <section style="background-color: #F4F4F4;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-8">@yield('content-s')</div>
+                <div class="col-md-3 col-sm-4 col-md-offset-1 sidebar" style="padding-top: 150px; padding-bottom: 50px;">
+                    @include('partials._blog_sidebar')
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12" style="background-color: #F4F4F4;">
+                    Latest Blog
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-12" style="background-color: #F4F4F4;">
-                Latest Blog
-            </div>
-        </div>
-    </div>
+    </section>
 @endsection
 
 @section('third_party_stylesheets')

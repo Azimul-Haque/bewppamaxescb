6e083c6945e779ae2f25e118026066bd1b58d654
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-17 19:10:57 +0600

    Commit Sun 09/17/2023 19:10:57.34 25058

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,23 +1,8 @@
-@extends('layouts.index')
+@extends('layouts.blog')
 @section('title') BCS Exam - বিসিএস পরীক্ষা | বিসিএস-সহ সরকারি চাকরির পরীক্ষার প্রস্তুতির জন্য সেরা অনলাইন প্ল্যাটফর্ম @endsection
 
 @section('third_party_stylesheets')
-<style type="text/css">
-  .pricing-style-fourteen .currency {
-      font-weight: 200;
-      color: var(--dark-3);
-      font-size: 22px;
-      position: absolute;
-      left: -40px;
-      top: 6px; 
-  }
-  .pricing-style-fourteen .amount {
-      font-size: 45px;
-  }
-  .pricing-style-fourteen .duration {
-      font-size: 16px;
-  }
-</style>
+
 @endsection
 
 @section('content')

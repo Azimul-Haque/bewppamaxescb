bdf42a20a53a0925470e2f12577a9df438186dcb
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-17 22:31:49 +0600

    Commit Sun 09/17/2023 22:31:49.25 13071

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -4,19 +4,7 @@
 @yield('third_party_stylesheets-s')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-sm-8">@yield('content-s')</div>
-            <div class="col-md-3 col-sm-4 col-md-offset-1 sidebar xs-margin-top-ten">
-                @include('partials._blog_sidebar')
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12" style="background-color: #F4F4F4;">
-                Latest Blog
-            </div>
-        </div>
-    </div>
+    sa
 @endsection
 
 @yield('third_party_scripts-s')

46b58e9cd14e098bfc23716b535ec6191cab322c
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-18 01:55:37 +0600

    Commit Mon 09/18/2023  1:55:37.32 20455

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -227,6 +227,7 @@ class BlogController extends Controller {
     public function getCategoryWise($name)
     {
         // SEO factor
+        dd($name);
         $name = ucwords(str_replace("-", " ", $name));
         // SEO factor
         $categories = Blogcategory::all();
@@ -244,7 +245,7 @@ class BlogController extends Controller {
           $blogs = Blog::whereIn('blogcategory_id', [13,14,15,16,17,18,10])->orderBy('id', 'desc')->paginate(7);
         } else {
           $category = Blogcategory::where('name', $name)->first();
-          dd($name);
+
           $blogs = Blog::where('blogcategory_id', $category->id)->orderBy('id', 'desc')->paginate(7);
         }
         

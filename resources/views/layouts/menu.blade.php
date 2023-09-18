d7df21ecad0e0f2be29771d7ef2fa92805cbb027
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-19 00:58:17 +0600

    Commit Tue 09/19/2023  0:58:17.42 3382

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -111,7 +111,7 @@
 <li class="nav-item">
     <a href="{{ route('dashboard.blogs') }}" class="nav-link {{ Request::is('dashboard/blogs') ? 'active' : '' }} {{ Request::is('dashboard/blogs/*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-pen-nib"></i>
-        <p>ব্লগসমূহ</p>
+        <p>ব্লগ</p>
     </a>
 </li>
 @endif

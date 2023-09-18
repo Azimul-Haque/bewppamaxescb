4f7009690c22302b0602edc8b8ef6ce584a81571
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-19 00:59:11 +0600

    Commit Tue 09/19/2023  0:59:11.28 9881

--- resources/views/dashboard/blogs/index.blade.php
+++ resources/views/dashboard/blogs/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title') ড্যাশবোর্ড | প্রশ্নব্যাংক @endsection
+@section('title') ড্যাশবোর্ড | ব্লগ @endsection
 
 @section('third_party_stylesheets')
 <link href="{{ asset('css/select2.min.css') }}" rel="stylesheet" />

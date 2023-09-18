e000f3be1e3dba3b5b6afd2d3ebbf1ea0f2b5239
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-19 01:53:08 +0600

    Commit Tue 09/19/2023  1:53:08.35 6216

--- resources/views/dashboard/blogs/index.blade.php
+++ resources/views/dashboard/blogs/index.blade.php
@@ -261,13 +261,13 @@
                                       <div class="modal-dialog" role="document">
                                       <div class="modal-content">
                                           <div class="modal-header bg-danger">
-                                          <h5 class="modal-title" id="deleteBlogModalLabel">প্রশ্ন ডিলেট</h5>
+                                          <h5 class="modal-title" id="deleteBlogModalLabel">ব্লগ ডিলেট</h5>
                                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                               <span aria-hidden="true">&times;</span>
                                           </button>
                                           </div>
                                           <div class="modal-body">
-                                            আপনি কি নিশ্চিতভাবে এই প্রশ্নটি ডিলেট করতে চান?<br/><br/>
+                                            আপনি কি নিশ্চিতভাবে এই ব্লগটি ডিলেট করতে চান?<br/><br/>
                                             <center>
                                                 <big><b>{{ $blog->question }}</b></big>
                                             </center>

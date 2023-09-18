6ada71b4c487b539e4f13742d4cf57c9143f78f6
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-19 02:18:15 +0600

    Commit Tue 09/19/2023  2:18:15.26 30962

--- resources/views/dashboard/blogs/index.blade.php
+++ resources/views/dashboard/blogs/index.blade.php
@@ -58,7 +58,7 @@
                           @foreach($blogs as $blog)
                               <tr>
                                   <td>
-                                      <a href="{{ route('blogs.single', $blog->slug) }}">{{ $blog->title }}</a>
+                                      <a href="{{ route('blog.single', $blog->slug) }}">{{ $blog->title }}</a>
                                       <br/>
                                       <span class="badge bg-success">{{ $blog->blogcategory->name }}</span>
                                       {{-- @foreach($blog->tags as $tag)

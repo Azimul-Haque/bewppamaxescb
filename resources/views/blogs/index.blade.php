9c9153a082d8c112a582f5a4a58deb72110cda8f
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-18 23:31:47 +0600

    Commit Mon 09/18/2023 23:31:47.30 10345

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -5,6 +5,10 @@
     
 @endsection
 
+@section('header-s')
+ব্লগসমূহ
+@endsection
+
 @section('content-s')
     <section style="padding-top: 50px; padding-bottom: 50px;">
         @foreach ($blogs as $blog)

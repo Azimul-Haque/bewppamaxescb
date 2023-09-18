6f059e87a4b10ca7f672cbaf6de63a459fdabb0f
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-18 23:28:55 +0600

    Commit Mon 09/18/2023 23:28:55.27 9289

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -69,7 +69,6 @@
             <div class="row">
                 <div class="col-md-12">
                     <h3>Sd asd</h3>
-                    <h2>Sd asd</h2>
                 </div>
             </div>
         </div>

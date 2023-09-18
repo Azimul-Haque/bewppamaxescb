35e24309f7e739be2efe34b75e0ad24dd0d29a76
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-18 23:00:29 +0600

    Commit Mon 09/18/2023 23:00:29.30 27846

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -65,6 +65,7 @@
 
 @section('content')
     <section style="background-color: #FAFAFA;">
+        asd
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-sm-8">@yield('content-s')</div>

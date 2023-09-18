31ef5827f18419bd2e074e11fba9683b777df57e
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-18 23:02:43 +0600

    Commit Mon 09/18/2023 23:02:43.34 21325

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -64,7 +64,7 @@
 @endsection
 
 @section('content')
-    <section style="background-color: #FFFFFF; height: 50px;">
+    <section style="background-color: #FFFFFF; height: 500px;">
         ASDASD
     </section>
     <section style="background-color: #FAFAFA;">

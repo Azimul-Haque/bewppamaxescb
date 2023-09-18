5bb16bb5bf3c426fe35dc17a6286de5ab9128c5f
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-19 02:26:56 +0600

    Commit Tue 09/19/2023  2:26:56.23 28504

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1263,7 +1263,7 @@ class DashboardController extends Controller
         $blog->title       = $request->title;
         $blog->user_id     = Auth::user()->id;
         dd($request->slug);
-        if(!empty($request->slug)) {
+        if($request->slug != null) {
             $blog->slug        = str_replace(['?',':', '\\', '/', '*', ' '], '-', strtolower($request->slug));
         } else {
             $blog->slug        = str_replace(['?',':', '\\', '/', '*', ' '], '-', strtolower($request->slug)) . '-' .time();

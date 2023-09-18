1afadbe107040d5e070296735bf5988313579ba5
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-18 23:27:01 +0600

    Commit Mon 09/18/2023 23:27:01.31 6546

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -64,7 +64,7 @@
 @endsection
 
 @section('content')
-    <section style="padding-top: 150px; padding-bottom: 100px;background-color: #FFFFFF; height: 100px;">
+    <section style="padding-top: 150px; padding-bottom: 70px;background-color: #FFFFFF; height: 100px;">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">ASDASd</div>

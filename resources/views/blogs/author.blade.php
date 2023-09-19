a15f833713ead1d751165cb1617dc0ccf3e67fb1
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-20 02:20:25 +0600

    Commit Wed 09/20/2023  2:20:25.32 27914

--- resources/views/blogs/author.blade.php
+++ resources/views/blogs/author.blade.php
@@ -27,7 +27,7 @@
 			            @endif
 			            <!-- end post image -->
 			            <div class="blog-details">
-			                <sma;; class="blog-date"><a href="{{ route('blogger.profile', $blog->user->id) }}"><b>{{ $blog->user->name }}</b></a> | {{ date('F d, Y', strtotime($blog->created_at)) }} | <a href="{{ route('blog.categorywise', str_replace(" ", "-", $blog->blogcategory->name)) }}">{{ $blog->blogcategory->name }}</a> </sma;;>
+			                <small class="blog-date"><a href="{{ route('blogger.profile', $blog->user->id) }}"><b>{{ $blog->user->name }}</b></a> | {{ date('F d, Y', strtotime($blog->created_at)) }} | <a href="{{ route('blog.categorywise', str_replace(" ", "-", $blog->blogcategory->name)) }}">{{ $blog->blogcategory->name }}</a> </small>
 			                <div class="blog-title">
 			                    <a href="{{ route('blog.single', $blog->slug) }}">
 			                        {{ $blog->title }}

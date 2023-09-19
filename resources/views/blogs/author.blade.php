958a9505bab216f9a7da48099ebe6350f0dc1034
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-20 02:48:20 +0600

    Commit Wed 09/20/2023  2:48:20.31 20947

--- resources/views/blogs/author.blade.php
+++ resources/views/blogs/author.blade.php
@@ -20,8 +20,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h4>{{ $blogger->name }}</h4>
-                s
+            	<center>
+                	<h4>{{ $blogger->name }}</h4>
+            	</center>
             </div>
         </div>
     </div>

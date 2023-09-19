3807cd8286b178829fac99fd0202a28c0e46c444
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-20 02:31:48 +0600

    Commit Wed 09/20/2023  2:31:48.35 27254

--- resources/views/blogs/author.blade.php
+++ resources/views/blogs/author.blade.php
@@ -22,7 +22,7 @@
 	    <div class="row">
 	    	@foreach($blogger->blogs as $blog)
 		     	<div class="col-md-4">
-					<div class="blog-listing blog-listing-classic no-margin-top wow fadeIn" style="padding: 20px; margin-bottom: 20px; background-color: #FFFFFF; border-radius: 5px; border: 1px solid #F1F1F1;">
+					<div class="blog-listing blog-listing-classic no-margin-top wow fadeIn" style="padding: 20px; margin-bottom: 20px; background-color: #FFFFFF; border-radius: 5px; border: 1px solid #F1F1F1; min-height: 400px;">
 			            <!-- post image -->
 			            @if($blog->featured_image != null)
 			                <div><a class="blog-image" href="{{ route('blog.single', $blog->slug) }}"><img class="img-responsive" src="{{ asset('images/blogs/'.$blog->featured_image) }}" alt="" style="width: 100%;" /></a></div><br/>

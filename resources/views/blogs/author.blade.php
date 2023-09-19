2e0a4f3967eff55ab3b8a176fc20d7e9242830c9
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-20 02:13:54 +0600

    Commit Wed 09/20/2023  2:13:54.22 7932

--- resources/views/blogs/author.blade.php
+++ resources/views/blogs/author.blade.php
@@ -6,16 +6,16 @@
 @endsection
 
 @section('content')
-	<section style="padding-top: 150px; padding-bottom: 60px;background-color: #FFFFFF; height: 100px; border-bottom: 1px solid #F4F4F4;">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h4>{{ $blogger->name }}</h4>
-                </div>
+<section style="padding-top: 150px; padding-bottom: 60px;background-color: #FFFFFF; height: 100px; border-bottom: 1px solid #F4F4F4;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4>{{ $blogger->name }}</h4>
             </div>
         </div>
-    </section>
-    <section style="background-color: #FAFAFA;">
+    </div>
+</section>
+<section style="background-color: #FAFAFA;">
     <div class="container">
 	    <div class="row">
 	      <div class="col-md-12 col-12">

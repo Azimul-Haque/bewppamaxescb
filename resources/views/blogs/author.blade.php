4f3a08a771f2bbf2ed5ba7c877622563c5fc2ae9
Azimul-Haque <orbachinujbuk@gmail.com>
2023-09-20 02:16:35 +0600

    Commit Wed 09/20/2023  2:16:35.29 5016

--- resources/views/blogs/author.blade.php
+++ resources/views/blogs/author.blade.php
@@ -19,7 +19,7 @@
     <div class="container">
 	    <div class="row">
 	    	@foreach($blogger->blogs as $blog)
-		     	<div class="col-md-4" style="background: Red;">
+		     	<div class="col-md-4" style="background: Red; margin: 10px;">
 					asd
 		    	</div>
 	    	@endforeach

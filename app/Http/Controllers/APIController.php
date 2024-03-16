2deb625895ebfa4e298170ca0d2c70ab11adb942
Azimul-Haque <orbachinujbuk@gmail.com>
2024-03-17 00:00:07 +0600

    Commit Sun 03/17/2024  0:00:07.32 690

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -32,10 +32,7 @@ class APIController extends Controller
 {
     public function test()
     {
-        $triedlastfivedays = Userotp::where('mobile', '01751398392')
-                                    ->where('created_at', '>=', Carbon::now()->subDays(5)->toDateTimeString())
-                                    ->count();
-         dd($triedlastfivedays);
+        
     } 
 
     public function generateOTP(Request $request)

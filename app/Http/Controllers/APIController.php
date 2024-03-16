0e2d7bbc21e07e1d4adf38b2bcf11d244833e21a
Azimul-Haque <orbachinujbuk@gmail.com>
2024-03-16 23:31:51 +0600

    Commit Sat 03/16/2024 23:31:51.27 12560

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -58,7 +58,9 @@ class APIController extends Controller
 
             // SPAM PREVENTION
             $triedlast24hours = Userotp::where('mobile', $mobile_number)
-                                       ->
+                                       ->where('created_at', '>', Carbon::now()->subDays(1))
+                                       ->groupBy(\DB::raw('HOUR(created_at)'))
+                                       ->count();
             
             // SPAM PREVENTION
 

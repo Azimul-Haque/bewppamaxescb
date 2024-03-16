82e2a9a0e9dddc25c1d9933a9043b0bc77635373
Azimul-Haque <orbachinujbuk@gmail.com>
2024-03-16 23:29:26 +0600

    Commit Sat 03/16/2024 23:29:26.26 27700

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -43,7 +43,7 @@ class APIController extends Controller
         ));
 
         if($request->softtoken == env('SOFT_TOKEN')) {
-            $triedlast24hours = 
+            $triedlast24hours = Userotp
             if()
             $pool = '0123456789';
             $otp = substr(str_shuffle(str_repeat($pool, 4)), 0, 4);

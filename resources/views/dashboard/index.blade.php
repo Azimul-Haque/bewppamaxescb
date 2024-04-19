fe5abf01be02fce01ff1f7559e8b4654016bf9f8
Azimul-Haque <orbachinujbuk@gmail.com>
2024-04-20 01:14:47 +0600

    Commit Sat 04/20/2024  1:14:47.27 17142

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -153,26 +153,22 @@
         data: {
             labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1,
-                pointBackgroundColor: "#fff",
+                label: '',
+                  borderColor: "#3e95cd",
+                  fill: true,
+                  data: {!! $totalsforchartc !!},
+                  borderWidth: 2,
+                  borderColor: "rgba(0,165,91,1)",
+                  borderCapStyle: 'butt',
+                  pointBorderColor: "rgba(0,165,91,1)",
+                  pointBackgroundColor: "#fff",
+                  pointBorderWidth: 1,
+                  pointHoverRadius: 5,
+                  pointHoverBackgroundColor: "rgba(0,165,91,1)",
+                  pointHoverBorderColor: "rgba(0,165,91,1)",
+                  pointHoverBorderWidth: 2,
+                  pointRadius: 5,
+                  pointHitRadius: 10,
             }]
         },
         options: {

bde7940c50207887d44c3647177a5cee651e677f
Azimul-Haque <orbachinujbuk@gmail.com>
2024-04-20 01:04:56 +0600

    Commit Sat 04/20/2024  1:04:56.35 18708

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -147,19 +147,19 @@
 @section('third_party_scripts')
   <script type="text/javascript">
     //-------------
-        //- LINE CHART -
-        //--------------
-        var lineChartCanvas = $('#lineChart').get(0).getContext('2d')
-        var lineChartOptions = $.extend(true, {}, areaChartOptions)
-        var lineChartData = $.extend(true, {}, areaChartData)
-        lineChartData.datasets[0].fill = false;
-        lineChartData.datasets[1].fill = false;
-        lineChartOptions.datasetFill = false
+    //- LINE CHART -
+    //--------------
+    var lineChartCanvas = $('#lineChart').get(0).getContext('2d')
+    var lineChartOptions = $.extend(true, {}, areaChartOptions)
+    var lineChartData = $.extend(true, {}, areaChartData)
+    lineChartData.datasets[0].fill = false;
+    lineChartData.datasets[1].fill = false;
+    lineChartOptions.datasetFill = false
 
-        var lineChart = new Chart(lineChartCanvas, {
-          type: 'line',
-          data: lineChartData,
-          options: lineChartOptions
-        })
+    var lineChart = new Chart(lineChartCanvas, {
+      type: 'line',
+      data: lineChartData,
+      options: lineChartOptions
+    })
   </script>
 @endsection

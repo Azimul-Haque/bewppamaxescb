d231a499d949d9e8e079243cddb0f10ee039335d
Azimul-Haque <orbachinujbuk@gmail.com>
2024-04-20 01:14:26 +0600

    Commit Sat 04/20/2024  1:14:26.24 9426

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -171,7 +171,8 @@
                     'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)'
                 ],
-                borderWidth: 1
+                borderWidth: 1,
+                pointBackgroundColor: "#fff",
             }]
         },
         options: {

f972af85423df69c2fbdf0efd2e13592bd3a5b5b
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-11 01:28:05 +0600

    Commit Sat 05/11/2024  1:28:05.31 26328

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -405,8 +405,8 @@ class APIController extends Controller
                 $courseexams = Courseexam::select('course_id', 'exam_id')
                                      ->where('course_id', $id)
                                      ->orderBy('exam_id', 'desc')
-                                     ->join('exams', 'exams.id', '=', 'courseexams.exam_id')
-                                     ->orderBy('exams.available_from')
+                                     // ->join('exams', 'exams.id', '=', 'courseexams.exam_id')
+                                     // ->orderBy('exams.available_from')
                                      // ->select('courseexams.*')
                                      ->get();
 

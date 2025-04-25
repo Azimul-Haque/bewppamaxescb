e8bbbfcc37aa5683ba3e8841a7646e6f26d9c810
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-04-25 16:02:02 +0600

    Commit Fri 04/25/2025 16:02:02.23 24806

--- resources/views/dashboard/exams/examstoday.blade.php
+++ resources/views/dashboard/exams/examstoday.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title') ড্যাশবোর্ড | পেমেন্ট @endsection
+@section('title') ড্যাশবোর্ড | আজ মোট পরীক্ষায় অংশগ্রহণ  @endsection
 
 @section('third_party_stylesheets')
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">

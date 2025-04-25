74e41ea5b9014235c2951f1909d55e3809a32ec9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-04-25 16:05:08 +0600

    Commit Fri 04/25/2025 16:05:08.24 688

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -50,7 +50,7 @@
               <div class="icon">
                 <i class="ion ion-person-add"></i>
               </div>
-              <a href="{{ route('dashboard.payments') }}" class="small-box-footer">পরীক্ষার্থী পাতা <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ route('dashboard.payments') }}" class="small-box-footer">পরীক্ষার্থী তালিকা <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->

0b743343843fd1396c8170fef79692cddf046480
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-04-25 16:02:22 +0600

    Commit Fri 04/25/2025 16:02:22.19 2123

--- resources/views/dashboard/exams/examstoday.blade.php
+++ resources/views/dashboard/exams/examstoday.blade.php
@@ -19,7 +19,7 @@
               <div class="card-tools">
                 <form class="form-inline form-group-lg" action="">
                   <div class="form-group">
-                    <input type="search-param" class="form-control form-control-sm" placeholder="পেমেন্ট খুঁজুন" id="search-param" required>
+                    <input type="search-param" class="form-control form-control-sm" placeholder="পরীক্ষায় অংশগ্রহণ খুঁজুন" id="search-param" required>
                   </div>
                   <button type="button" id="search-button" class="btn btn-default btn-sm" style="margin-left: 5px;">
                     <i class="fas fa-search"></i> খুঁজুন

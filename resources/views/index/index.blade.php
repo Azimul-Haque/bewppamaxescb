176e7dcfeae3d8403ec71fa67ce724509d41bafc
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-04-26 02:39:11 +0600

    Commit Sat 04/26/2025  2:39:11.25 26883

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -39,6 +39,7 @@
               <a href='https://play.google.com/store/apps/details?id=com.orbachinujbuk.bcs' target="_blank"><img alt='Get it on Google Play' class="img-responsive" style="max-width: 250px; width: auto;" src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
             </div>
           </div>
+          আ
           <!-- Modal footer -->
           <div class="modal-footer">
               <button type="button" class="btn btn-danger" data-bs-dismiss="modal">বন্ধ করুন</button>

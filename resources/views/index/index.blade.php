8091952d29b0e95678f851a81eac91a9be29a4c2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-06-13 19:54:29 +0600

    Commit Fri 06/13/2025 19:54:29.26 12220

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -473,7 +473,7 @@
                             @endif
                         </p>
                     </div>
-                    <div class="light-rounded-buttons" style="margin-top: 20px;">
+                    <div class="light-rounded-buttons" style="margin-top: 20px; margin-bottom: 20px;">
                       <center><a href="{{ route('blog.single', $blog->slug) }}" class="btn primary-btn-outline">বিস্তারিত পড়ুন</a></center>
                     </div>
                 </div>

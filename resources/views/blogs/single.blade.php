61a55a6f7b84cebe911c996683fcd5f899276262
azimul/acl-pc <orbachinujbuk@gmail.com>
2025-09-23 19:58:43 +0600

    Commit Tue 09/23/2025 19:58:43.21 10363

--- resources/views/blogs/single.blade.php
+++ resources/views/blogs/single.blade.php
@@ -7,6 +7,10 @@
     @else
         <meta property="og:image" content="{{ asset('images/abc.png') }}" />
     @endif
+    // এখানে কাজ হবে
+    // এখানে কাজ হবে
+    // এখানে কাজ হবে
+    // এখানে কাজ হবে
     <meta name="keywords" content="BCS, বিসিএস, bcs book list, bcs book suggestion, BCS Preparation Books, বিসিএস প্রিলিমিনারি বই তালিকা, বিসিএস বই তালিকা, বিসিএস লিখিত বই তালিকা, bcs preliminary book list, bcs written book list, বিসিএস প্রিলিমিনারি পরীক্ষার সিলেবাস, বিসিএস পরীক্ষার সিলেবাস">
     <meta property="og:title" content="{{ $blog->title }}"/>
     <meta name="description" property="og:description" content="{{ mb_substr(strip_tags($blog->body), 0, 200) }}..." />

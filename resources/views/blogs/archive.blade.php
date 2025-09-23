1eb078891b4e52ab633156a717a3ec14a2975d46
azimul/acl-pc <orbachinujbuk@gmail.com>
2025-09-23 19:34:29 +0600

    Commit Tue 09/23/2025 19:34:29.21 7420

--- resources/views/blogs/archive.blade.php
+++ resources/views/blogs/archive.blade.php
@@ -2,7 +2,7 @@
 @section('title-secondary') {{ $archivedate }} | ব্লগ-Blog | BCS Exam - বিসিএস পরীক্ষা | বিসিএস-সহ সরকারি চাকরির পরীক্ষার প্রস্তুতির জন্য সেরা অনলাইন প্ল্যাটফর্ম @endsection
 
 @section('meta-data')
-
+    
 @endsection
 
 @section('third_party_stylesheets-s')

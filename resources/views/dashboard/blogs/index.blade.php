60e091a14fe6be92fea065e94ce7ff02611ec5fb
azimul/acl-pc <orbachinujbuk@gmail.com>
2025-09-24 18:25:47 +0600

    Commit Wed 09/24/2025 18:25:47.24 25594

--- resources/views/dashboard/blogs/index.blade.php
+++ resources/views/dashboard/blogs/index.blade.php
@@ -341,7 +341,7 @@
                     <input type="text" name="slug" value="{{ old('slug') }}" class="form-control mb-3" placeholder="ব্লগ স্লাগ (Optional)">
                     <div class="row">
                         <div class="col-md-6">
-                          <input type="text" name="keywords" value="{{ old('keywords') }}" class="form-control mb-3" placeholder="ব্লগ স্লাগ (Optional)">
+                          <input type="text" name="keywords" value="{{ old('keywords') }}" class="form-control mb-3" placeholder="কি-ওয়ার্ড (Optional)">
                         </div>
                         <div class="col-md-6">
                           <input type="text" name="description" value="{{ old('description') }}" class="form-control mb-3" placeholder="ব্লগ স্লাগ (Optional)">

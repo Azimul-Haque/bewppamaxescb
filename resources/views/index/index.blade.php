6984d964fba72700d63da7a4b0361567fc4e8be6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-21 04:37:05 +0600

    Commit Tue 10/21/2025  4:37:05.25 4061

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -634,13 +634,13 @@
     <!-- ========================= contact-section end ========================= -->
 
     <!-- ========================= map-section end ========================= -->
-    {{-- <section class="map-section map-style-9">
+    <section class="map-section map-style-9">
       <div class="map-container">
         <object style="border:0; height: 500px; width: 100%;"
           data="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14606.067929552457!2d90.35101528681905!3d23.76459799164188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c09f9ba3d447%3A0x1babce9f1c6c95a3!2sMohammadpur%2C%20Dhaka!5e0!3m2!1sen!2sbd!4v1655394465359!5m2!1sen!2sbd"></object>
       </div>
       </div>
-    </section> --}}
+    </section>
     <!-- ========================= map-section end ========================= -->
 @endsection
 

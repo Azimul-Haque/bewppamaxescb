3b89a9be692ef430725b72a2db5da20950993584
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-21 05:26:34 +0600

    Commit Tue 10/21/2025  5:26:34.32 11150

--- resources/views/index/documentation.blade.php
+++ resources/views/index/documentation.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.index')
-@section('title') Privacy Policy | BCS Exam AID | বিসিএস-সহ সরকারি চাকরির পরীক্ষার প্রস্তুতির জন্য সেরা অনলাইন প্ল্যাটফর্ম @endsection
+@section('title') ডকুমেন্টেশন | BCS Exam AID | বিসিএস-সহ সরকারি চাকরির পরীক্ষার প্রস্তুতির জন্য সেরা অনলাইন প্ল্যাটফর্ম @endsection
 
 @section('third_party_stylesheets')
 	<!-- Structured data JSON-LD (optional but highly recommended) -->

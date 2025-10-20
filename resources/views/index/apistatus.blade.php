884da86b26f717954fc9bb7294def784228283f6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-21 04:45:32 +0600

    Commit Tue 10/21/2025  4:45:32.26 12565

--- resources/views/index/apistatus.blade.php
+++ resources/views/index/apistatus.blade.php
@@ -9,12 +9,12 @@
     <!-- Load Bootstrap v5.0.1 CSS from CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" xintegrity="sha384-+0n0xVW2eSR5OomGNYDnhZAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
-      <meta name="mobile-web-app-capable" content="yes">
-      <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('images/favicon.png') }}">
-      <meta name="theme-color" content="#155BD5">
-      <meta name="msapplication-navbutton-color" content="#155BD5">
-      <meta name="apple-mobile-web-app-status-bar-style" content="#155BD5">
-      <meta name="author" content="A. H. M. Azimul Haque">
+    <meta name="mobile-web-app-capable" content="yes">
+    <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('images/favicon.png') }}">
+    <meta name="theme-color" content="#155BD5">
+    <meta name="msapplication-navbutton-color" content="#155BD5">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#155BD5">
+    <meta name="author" content="A. H. M. Azimul Haque">
     <!-- Custom styling for better presentation and font -->
     <style>
         body {

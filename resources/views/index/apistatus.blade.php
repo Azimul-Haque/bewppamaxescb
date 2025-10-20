685c28189fc664d5a64fb5918921827695c99785
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-21 04:50:57 +0600

    Commit Tue 10/21/2025  4:50:57.24 5044

--- resources/views/index/apistatus.blade.php
+++ resources/views/index/apistatus.blade.php
@@ -58,7 +58,7 @@
         {
         "@context": "https://schema.org",
         "@type": "Website",
-        "headline": "BCS Exam Aid",
+        "headline": "BCS Exam Aid - API Status",
         "description": "BCS Exam Aid - বিসিএস ও সরকারি চাকরির সেরা প্ল্যাটফর্ম",
         "image": "{{ asset('images/bcs-exam-aid-banner.png') }}",
         "url": "{{ url()->current() }}",

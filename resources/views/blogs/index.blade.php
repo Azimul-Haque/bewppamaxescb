60241d4c376a5d30f80e52b9683e3460d3a6004f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-21 05:04:32 +0600

    Commit Tue 10/21/2025  5:04:32.23 17497

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -4,6 +4,23 @@
 @section('meta-data')
     <meta name="description" content="BCS Exam Aid বাংলাদেশ সিভিল সার্ভিস (BCS) পরীক্ষা এবং অন্যান্য সরকারি চাকরি (NSI, দুদক, বাংলাদেশ ব্যাংক, অন্যান্য ব্যাংক, প্রাথমিক শিক্ষক নিয়োগ) পরীক্ষার প্রস্তুতির জন্য সেরা ডেডিকেটেড অনলাইন প্ল্যাটফর্ম। Developed By A. H. M. Azimul Haque.">
     <meta name="keywords" content="BCS, Bangladesh Civil Service, NSI, দুদক, বাংলাদেশ ব্যাংক, অন্যান্য ব্যাংক, প্রাথমিক শিক্ষক নিয়োগ, Primary Exam, Job Circular, Bank Job Circular, Bank Job Exam, BCS Circular, বিসিএস পরীক্ষা, বার কাউন্সিল পরীক্ষা, জুডিশিয়াল পরীক্ষা, Judicial Exam, bcs book list, bcs book suggestion, BCS Preparation Books, বিসিএস প্রিলিমিনারি বই তালিকা, বিসিএস বই তালিকা, বিসিএস লিখিত বই তালিকা, bcs preliminary book list, bcs written book list, বিসিএস প্রিলিমিনারি পরীক্ষার সিলেবাস, বিসিএস পরীক্ষার সিলেবাস">
+    <!-- Structured data JSON-LD (optional but highly recommended) -->
+    <script type="application/ld+json">
+        {
+        "@context": "https://schema.org",
+        "@type": "Article",
+        "headline": "{{ $blog->title }}",
+        "description": "{{ $blog->description ?? mb_substr(strip_tags($blog->body), 0, 200) }}",
+        "image": "{{ asset('images/blogs/'.$blog->featured_image) ?? asset('images/bcs-exam-aid-banner.png') }}",
+        "url": "{{ url()->current() }}",
+        "author": {
+          "@type": "Person",
+          "name": "{{ $blog->user->name ?? 'এ. এইচ. এম. আজিমুল হক' }}"
+        },
+        "datePublished": "{{ $blog->created_at ?? now()->toIso8601String() }}",
+        "dateModified": "{{ $blog->updated_at ?? now()->toIso8601String() }}"
+        }
+    </script>
 @endsection
 
 @section('third_party_stylesheets-s')

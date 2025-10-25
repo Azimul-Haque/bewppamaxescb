ff92e3cbd9d468324643d03c2f0ac9fbff4cf9a5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-26 00:08:43 +0600

    Commit Sun 10/26/2025  0:08:43.28 28994

--- app/Models/Meritlist.php
+++ app/Models/Meritlist.php
@@ -1,20 +1,20 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Meritlist extends Model
 {
     public function exam(){
-        return $this->belongsTo('App\Exam');
+        return $this->belongsTo('App\Models\Exam');
     }
 
     public function course(){
-        return $this->belongsTo('App\Course');
+        return $this->belongsTo('App\Models\Course');
     }
 
     public function user(){
-        return $this->belongsTo('App\User');
+        return $this->belongsTo('App\Models\User');
     }
 }

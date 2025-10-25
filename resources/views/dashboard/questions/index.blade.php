87b489941251e8048c25ee5de748944799c83508
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-26 01:18:09 +0600

    Commit Sun 10/26/2025  1:18:09.30 9920

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -665,9 +665,7 @@
                       </div>
                       <div class="col-md-6">
                           <select name="tags_ids[]" class="form-control multiple-select" multiple="multiple" data-placeholder="ট্যাগ">
-                              @foreach ($tags as $tag)
-                                  <option value="{{ $tag->id }}">{{ $tag->name }}</option>
-                              @endforeach
+                              
                           </select>
                       </div>
                     </div>

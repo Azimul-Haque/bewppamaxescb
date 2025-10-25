9f43276c91a0916cfe046c7c93d4fff158851f7f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-26 00:20:08 +0600

    Commit Sun 10/26/2025  0:20:08.22 7079

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        Paginator::useBootstrap();
+        if (method_exists(Paginator::class, 'useBootstrap')) {
+            Paginator::useBootstrap(); 
+        }
     }
 }

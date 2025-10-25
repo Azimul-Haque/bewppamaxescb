51d8d82a1d096404a9cbefd5c72b3bdd3637f510
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-26 00:09:43 +0600

    Commit Sun 10/26/2025  0:09:43.29 16538

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
     public function user(){
-        return $this->belongsTo('App\User');
+        return $this->belongsTo('App\Models\User');
     }
 
     public function blogcategory() {
-      return $this->belongsTo('App\Blogcategory');
+      return $this->belongsTo('App\Models\Blogcategory');
     }
 }

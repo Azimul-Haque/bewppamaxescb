459f05aff288a13d261dba58df77acb760e92e46
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-26 00:09:36 +0600

    Commit Sun 10/26/2025  0:09:36.28 25937

--- app/Models/Blogcategory.php
+++ app/Models/Blogcategory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,6 +9,6 @@ class Blogcategory extends Model
     public $timestamps = false;
 
     public function blogs() {
-      return $this->hasMany('App\Blog');
+      return $this->hasMany('App\Models\Blog');
     } 
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,11 +8,11 @@ class Category extends Model
 {
     public $timestamps = false;
 
-    public function site(){
-        return $this->belongsTo('App\Models\Site');
-    }
+    // public function site(){
+    //     return $this->belongsTo('App\Models\Site');
+    // }
 
-    public function expenses(){
-        return $this->hasMany('App\Models\Expense');
-    }
+    // public function expenses(){
+    //     return $this->hasMany('App\Models\Expense');
+    // }
 }

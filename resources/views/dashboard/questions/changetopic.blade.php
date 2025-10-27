19764b94d6d099da0cee9f0cff401a96c39e7535
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-28 01:40:41 +0600

    Commit Tue 10/28/2025  1:40:41.28 21064

--- resources/views/dashboard/questions/changetopic.blade.php
+++ resources/views/dashboard/questions/changetopic.blade.php
@@ -93,10 +93,10 @@
                                   </td>                              
                                   <td>
                                     <select class="form-control form-control-sm topic-select-field" name="topicchangeid" id="topicchangeid{{ $question->id }}" data-saved-id="{{ $question->id }}" data-saved-text="{{ $question->topic->full_path ?? '' }}">
-                                      <option selected disabled>টপিক সিলেক্ট করুন</option>
+                                      {{-- <option selected disabled>টপিক সিলেক্ট করুন</option>
                                       @foreach($topics as $topic)
                                         <option value="{{ $topic->id }}" @if($topic->id == $question->topic_id) selected @endif>{{ $topic->name }}</option>
-                                      @endforeach
+                                      @endforeach --}}
                                     </select>
                                   </td>
                                   <td>

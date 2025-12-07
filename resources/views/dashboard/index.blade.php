48c7a8b2c3069b6d75bd4270a5f37da91c741ab2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-07 23:41:51 +0600

    Commit Sun 12/07/2025 23:41:51.25 8018

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -120,6 +120,34 @@
             </div>
             {{-- Modal Code --}}
             {{-- Modal Code --}}
+
+            <button class="btn btn-warning" data-toggle="modal" data-target="#clearQueryCacheModal">
+              <i class="fas fa-tools"></i> সকল কোয়েরি ক্যাশ (API) ক্লিয়ার করুন
+            </button>
+            {{-- Modal Code --}}
+            {{-- Modal Code --}}
+            <!-- Modal -->
+            <div class="modal fade" id="clearQueryCacheModal" tabindex="-1" role="dialog" aria-labelledby="clearQueryCacheModalLabel" aria-hidden="true" data-backdrop="static">
+                <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header bg-warning">
+                    <h5 class="modal-title" id="clearQueryCacheModalLabel">কোয়েরি ক্যাশ ক্লিয়ার</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    </div>
+                    <div class="modal-body">
+                      আপনি কি নিশ্চিতভাবে সকল কোয়েরি ক্যাশ ক্লিয়ার করতে চান?<br/>
+                    </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
+                    <a href="{{ route('dashboard.clearquerycache') }}" class="btn btn-warning">ক্যাশ ক্লিয়ার করুন</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+            {{-- Modal Code --}}
+            {{-- Modal Code --}}
             <br/>
             <br/>
           </div>

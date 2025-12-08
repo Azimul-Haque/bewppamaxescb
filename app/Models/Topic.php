5fd93b04ebce910cd9b6fcd3712298ad913ef888
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-09 03:45:03 +0600

    Commit Tue 12/09/2025  3:45:03.28 24026

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -45,7 +45,7 @@ class Topic extends Model
         // This is the simplest way without using a dedicated Tree package (like Nested Set).
         $descendantIds = $this->getDescendantIds([$this->id]);
 
-        dd($descendantIds);
+        // dd($descendantIds);
         
         // 2. Count all questions where the topic_id is in the list of descendant IDs.
         return DB::table('questions')

99c5632ce8c2b3891ee951bcf2b6ab4ef68910ab
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-15 02:18:56 +0600

    Commit Mon 12/15/2025  2:18:56.30 14944

--- resources/views/dashboard/courses/index.blade.php
+++ resources/views/dashboard/courses/index.blade.php
@@ -125,12 +125,12 @@
                                                   <div class="input-group mb-3">
                                                     <select name="category" class="form-control" required>
                                                         <option selected="" disabled="" value="">আপডেটেড ধরন</option>
-                                                        <option value="1" @if($course->type == 4) selected @endif>বিসিএস</option>
-                                                        <option value="2" @if($course->type == 4) selected @endif>প্রাইমারি</option>
-                                                        <option value="3" @if($course->type == 4) selected @endif>ব্যাংক</option>
+                                                        <option value="1" @if($course->type == 1) selected @endif>বিসিএস</option>
+                                                        <option value="2" @if($course->type == 2) selected @endif>প্রাইমারি</option>
+                                                        <option value="3" @if($course->type == 3) selected @endif>ব্যাংক</option>
                                                         <option value="4" @if($course->type == 4) selected @endif>NTRCA</option>
-                                                        <option value="5" @if($course->type == 4) selected @endif>NSI, DGFI ও অন্যান্য</option>
-                                                        <option value="6" @if($course->type == 4) selected @endif>প্রশ্ন ব্যাংক</option>
+                                                        <option value="5" @if($course->type == 5) selected @endif>NSI, DGFI ও অন্যান্য</option>
+                                                        <option value="6" @if($course->type == 6) selected @endif>প্রশ্ন ব্যাংক</option>
                                                     </select>
                                                     <div class="input-group-append">
                                                         <div class="input-group-text"><span class="fas fa-tag"></span></div>

f5c254c9f95fad298d4ec317ff71ca969831dfc2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-15 04:56:03 +0600

    Commit Mon 12/15/2025  4:56:03.30 16721

--- routes/web.php
+++ routes/web.php
@@ -157,6 +157,7 @@ Route::get('/dashboard/courses/{id}/delete', 'CourseController@deleteCourse')->n
 Route::get('/dashboard/courses/add/exam/{id}', 'CourseController@addExamToCourse')->name('dashboard.courses.add.exam');
 Route::post('/dashboard/courses/add/exam/store', 'CourseController@storeCourseExam')->name('dashboard.courses.exam.store');
 Route::get('/dashboard/courses/exams/serial/{id}/update', 'CourseController@editExamSerialCourse')->name('dashboard.courses.exam.serial.edit');
+Route::get('/dashboard/courses/exams/serial/{id}/update', 'CourseController@editExamSerialCourse')->name('dashboard.courses.exam.serial.edit');
 
 Route::get('/dashboard/materials', 'MaterialController@getMaterials')->name('dashboard.materials');
 Route::post('/dashboard/materials/store', 'MaterialController@storeMaterial')->name('dashboard.materials.store');

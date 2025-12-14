a84383168d0eaa29fe21c67a5368450f868d6e8f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-15 02:13:27 +0600

    Commit Mon 12/15/2025  2:13:27.32 18325

--- database/migrations/2022_06_29_004217_create_courses_table.php
+++ database/migrations/2022_06_29_004217_create_courses_table.php
@@ -19,7 +19,7 @@ class CreateCoursesTable extends Migration
             $table->integer('status')->default(0);
             $table->integer('type')->default(1); // 1 = Course, 2 = BJS MT, 3 = Bar MT, 4 = Free MT
             $table->integer('priority')->default(3);
-            $table->integer('category')->default(0); // 1 = BCS, 2 = Primary, 3 = Bank, 4 = NTRCS, 5 = NSI/DGFI and Others
+            $table->integer('category')->default(0); // 1 = BCS, 2 = Primary, 3 = Bank, 4 = NTRCS, 5 = NSI/DGFI and Others, 6 = QB
             $table->integer('live')->default(0); // live থাকলে কোর্স ক্যাটাগরির ভেতরে শো করবে
             $table->integer('serial')->default(0); // priority ব্যবহৃত হবে চলমান কোর্সসমূহ বার এ, serial ব্যবহৃত হবে কোর্স্ ক্যাটাগরিতে
             $table->timestamps();

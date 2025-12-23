7d170d3f1e621eee0c233ea9662a1308076ded0e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-24 01:55:21 +0600

    Commit Wed 12/24/2025  1:55:21.23 23736

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -162,6 +162,7 @@ class APIController extends Controller
                     return $otp; 
                 } else {
                     return 'Requested within 30 minutes!';
+                    return response()->json(['success' => false, 'message' => 'আপনি বার বার চেষ্টা করছেন! পরে চেষ্টা করুন।'], 429);
                 }
                 // SPAM PREVENTION Layer 2
             } else {

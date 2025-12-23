c526f5826b028d4ac36184afd3bb3caedc6bb1c8
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-24 04:13:02 +0600

    Commit Wed 12/24/2025  4:13:02.23 16571

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/testapi', 'APIController@test')->name('api.test');
 
-// Route::post('/generateotp', 'APIController@generateOTP')->name('api.generateotp');
-Route::post('/generateotp', 'APIController@generateOTP')->name('api.generateotp')->middleware('otp.throttle');
+Route::post('/generateotp', 'APIController@generateOTP')->name('api.generateotp');
+// Route::post('/generateotp', 'APIController@generateOTP')->name('api.generateotp')->middleware('otp.throttle');
 Route::post('/loginorcreate', 'APIController@loginOrCreate')->name('api.loginorcreate');
 
 Route::get('/checkuid/{softtoken}/{phonenumber}', 'APIController@checkUid')->name('api.checkuid');

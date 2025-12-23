8a87a0521020ba8493f82a7bc778a9dbc1206f0f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-24 01:50:40 +0600

    Commit Wed 12/24/2025  1:50:40.26 28505

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
         'otp.throttle' => [
-            'throttle:1,5', // Allows 3 attempts every 20 minutes per IP
+            'throttle:1,5', // Allows 1 attempts every 5 minutes per IP
         ],
     ];
 

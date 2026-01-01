d05e2b5c9a3b9e7e97dc3a7d857738505c953934
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-02 05:36:45 +0600

    Commit Fri 01/02/2026  5:36:45.27 5891

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -1,84 +1,85 @@
 @extends('layouts.index')
 @section('title') @yield('title-secondary') @endsection
 
+@section('meta-data')
+    @yield('meta-data')
+@endsection
+
 @section('third_party_stylesheets')
     @yield('third_party_stylesheets-s')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
-          integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
-          crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    
     <style type="text/css">
-        .blog-image {
-            overflow: hidden;
-            background: rgb(161, 161, 161);
+        /* Global Blog Styles */
+        body {
+            background-color: #f4f7f6;
         }
-        .col-md-offset-1 {
-            margin-left: 8.33333333%;
+        .blog-header-section {
+            padding-top: 130px; 
+            padding-bottom: 30px;
+            background-color: #ffffff; 
+            border-bottom: 1px solid #eeeeee;
         }
-        .blog-title, .blog-title a {
-            font-size: 30px;
-            font-weight: bold;
-            color: #000000;
-        }
-        .separator-line {
-            height: 2px;
-            margin: 0 auto;
-            width: 30px;
-            margin: 1% auto;
-        }
-        .blog-like, .blog-share, .comment {
-            font-size: 12px;
+        .blog-header-title {
+            font-weight: 800;
+            color: #2c3e50;
+            margin: 0;
+            font-size: 28px;
             text-transform: uppercase;
-            letter-spacing: 2px;
-            margin-right: 25px;
-        }
-        .btn.btn-small {
-            font-size: 11px;
-            padding: 2px 14px;
             letter-spacing: 1px;
-            border-radius: 0px;
         }
-        .highlight-button {
-            border: 2px solid #000;
-            display: inline-block;
-            padding: 8px 20px 9px;
-            font-size: 12px;
-            color: #000;
-            background-color: transparent;
+        .main-content-area {
+            padding-top: 50px;
+            padding-bottom: 80px;
         }
-        /*.widget-posts li {
-            padding: 10px 0;
+        
+        /* Sidebar Specific */
+        .sidebar-wrapper {
+            padding-top: 0; /* মোবাইল ফ্রেন্ডলি করার জন্য টপ প্যাডিং কমানো হয়েছে */
         }
-        .widget-posts-details {
-            position: relative;
-            overflow: hidden;
-            top: -4px;
-            font-size: 12px;
-            line-height: 14px;
-        }*/
-        .overflowellipsis {
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
+
+        @media (min-width: 992px) {
+            .sidebar-wrapper {
+                padding-left: 30px; /* ডেস্কটপে কন্টেন্ট থেকে সাইডবারের দূরত্ব */
+            }
+        }
+
+        @media (max-width: 768px) {
+            .blog-header-section {
+                padding-top: 100px;
+            }
+            .blog-header-title {
+                font-size: 22px;
+                text-align: center;
+            }
         }
     </style>
 @endsection
 
 @section('content')
-    <section style="padding-top: 150px; padding-bottom: 60px;background-color: #FFFFFF; height: 100px; border-bottom: 1px solid #F4F4F4;">
+    <section class="blog-header-section">
         <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h4>@yield('header-s')</h4>
+            <div class="row align-items-center">
+                <div class="col-12">
+                    <h4 class="blog-header-title">
+                        <i class="fas fa-feather-alt text-primary me-2"></i>@yield('header-s')
+                    </h4>
                 </div>
             </div>
         </div>
     </section>
-    <section style="background-color: #FAFAFA;">
+
+    <section class="main-content-area">
         <div class="container">
             <div class="row">
-                <div class="col-md-8 col-sm-8">@yield('content-s')</div>
-                <div class="col-md-3 col-sm-4 col-md-offset-1 sidebar" style="padding-top: 150px; padding-bottom: 50px;">
-                    @include('partials._blog_sidebar')
+                <div class="col-lg-8 col-md-12">
+                    @yield('content-s')
+                </div>
+
+                <div class="col-lg-4 col-md-12 mt-5 mt-lg-0">
+                    <div class="sidebar-wrapper">
+                        @include('partials._blog_sidebar')
+                    </div>
                 </div>
             </div>
         </div>

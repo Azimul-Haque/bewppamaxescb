f80d47b943204022742a71c440ac89c1e3a96360
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-02 05:25:52 +0600

    Commit Fri 01/02/2026  5:25:52.30 21624

--- resources/views/blogs/categorywise.blade.php
+++ resources/views/blogs/categorywise.blade.php
@@ -98,51 +98,64 @@
 
 @section('content-s')
     <section style="padding-top: 50px; padding-bottom: 50px;">
-        @foreach ($blogs as $blog)
-        <div class="blog-listing blog-listing-classic no-margin-top wow fadeIn">
-            <!-- post image -->
-            @if($blog->featured_image != null)
-                <div><a class="blog-image" href="{{ route('blog.single', $blog->slug) }}"><img class="img-responsive" src="{{ asset('images/blogs/'.$blog->featured_image) }}" alt="" style="width: 100%;" /></a></div><br/>
-            @endif
-            <!-- end post image -->
-            <div class="blog-details">
-                <div class="blog-date">Posted by <a href="{{ route('blogger.profile', $blog->user->id) }}"><b>{{ $blog->user->name }}</b></a> | {{ date('F d, Y', strtotime($blog->created_at)) }} | <a href="{{ route('blog.categorywise', $blog->blogcategory->name) }}">{{ $blog->blogcategory->name }}</a> </div>
-                <div class="blog-title"><a href="{{ route('blog.single', $blog->slug) }}">
-                    {{ $blog->title }}
-                </a></div>
-                <div style="text-align: justify;">
-                    @if(strlen(strip_tags($blog->body))>600)
-                        {{ mb_substr(strip_tags($blog->body), 0, stripos($blog->body, " ", stripos(strip_tags($blog->body), " ")+500))."... " }}
-                    @else
-                        {{ strip_tags($blog->body) }}
-                    @endif
+        <div class="container">
+            <div class="row">
+                @foreach ($blogs as $blog)
+                <div class="col-md-6 col-sm-12" style="padding-bottom: 25px;">
+                    <div class="blog-card wow fadeIn">
+                        <div class="blog-image-wrapper">
+                            <a href="{{ route('blog.single', $blog->slug) }}">
+                                @if($blog->featured_image != null)
+                                    <img src="{{ asset('images/blogs/'.$blog->featured_image) }}" alt="{{ $blog->title }}" />
+                                @else
+                                    <img src="{{ asset('images/default-blog.png') }}" alt="Default Image" />
+                                @endif
+                            </a>
+                        </div>
+                        
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <i class="far fa-user"></i> {{ $blog->user->name }} &nbsp;|&nbsp; 
+                                <i class="far fa-calendar-alt"></i> {{ bangla(date('M d, Y', strtotime($blog->created_at))) }}
+                            </div>
+                            
+                            <h3 class="blog-card-title">
+                                <a href="{{ route('blog.single', $blog->slug) }}">{{ $blog->title }}</a>
+                            </h3>
+                            
+                            <div class="blog-excerpt">
+                                @if($blog->description != '')
+                                  {{ $blog->description }}
+                                @else
+                                  @if(strlen(strip_tags($blog->body))>200)
+                                      {{ mb_substr(strip_tags($blog->body), 0, stripos($blog->body, " ", stripos(strip_tags($blog->body), " ")+200))."... " }}
+                                  @else
+                                      {{ strip_tags($blog->body) }}
+                                  @endif
+                                @endif
+                            </div>
+                            
+                            <div class="mt-auto">
+                                <div class="blog-footer">
+                                    <div>
+                                        <span class="text-danger"><i class="far fa-heart"></i> {{ $blog->likes }}</span>
+                                        <span class="ms-2 text-primary"><i class="far fa-folder"></i> <a href="{{ route('blog.categorywise', $blog->blogcategory->name) }}" title="">{{ $blog->blogcategory->name }}</a></span>
+                                    </div>
+                                    <a class="read-more-btn" href="{{ route('blog.single', $blog->slug) }}">আরও পড়ুন »</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="separator-line bg-black no-margin-lr margin-four"></div>
-                <div style="margin-bottom: 10px;">
-                    <a href="#!" class="blog-like"><i class="far fa-heart"></i> {{ $blog->likes }} Like(s)</a>
-                    <a href="#!" class="comment"><i class="far fa-comment"></i>
-                    <span id="comment_count{{ $blog->id }}"></span>
-                     comment(s)</a>
+                @endforeach
+            </div>
+
+            <div class="row">
+                <div class="col-12 text-center mt-4">
+                    {{ $blogs->links() }}
                 </div>
-                <a class="highlight-button btn btn-small xs-no-margin-bottom" href="{{ route('blog.single', $blog->slug) }}">Read More »</a>
-                </br>
-                </br>
             </div>
         </div>
-        <script type="text/javascript" src="{{ asset('vendor/hcode/js/jquery.min.js') }}"></script>
-        <script type="text/javascript">
-            // $.ajax({
-            //     url: "https://graph.facebook.com/v2.2/?fields=share{comment_count}&id={{ url('/blog/'.$blog->slug) }}",
-            //     dataType: "jsonp",
-            //     success: function(data) {
-            //         $('#comment_count{{ $blog->id }}').text(data.share.comment_count);
-            //     }
-            // });
-        </script>
-        @endforeach
-        <!-- end post item -->
-        {{-- paginating --}}
-        {{ $blogs->links() }}
     </section>
 @endsection
 

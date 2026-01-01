3d7ad5ecaa94bdca37c528f362c85c11772338f4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-02 05:43:08 +0600

    Commit Fri 01/02/2026  5:43:08.29 8969

--- resources/views/blogs/single.blade.php
+++ resources/views/blogs/single.blade.php
@@ -72,6 +72,80 @@
 @endsection
 
 @section('content-s')
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/bn_IN/sdk.js#xfbml=1&version=v12.0&appId=163879201229487"></script>
+
+    <div class="blog-single-wrapper">
+        <h1 class="mb-3 font-weight-bold" style="color: #222; font-size: 32px;">{{ $blog->title }}</h1>
+        
+        <div class="mb-4 text-muted" style="font-size: 14px;">
+            <i class="far fa-user"></i> <a href="{{ route('blogger.profile', $blog->user->id) }}"><b>{{ $blog->user->name }}</b></a> 
+            <span class="mx-2">|</span>
+            <i class="far fa-calendar-alt"></i> {{ date('F d, Y', strtotime($blog->created_at)) }} 
+            <span class="mx-2">|</span>
+            <i class="far fa-folder"></i> <a href="{{ route('blog.categorywise', str_replace(' ', '-', $blog->blogcategory->name)) }}">{{ $blog->blogcategory->name }}</a>
+        </div>
+
+        @if($blog->featured_image != null)
+            <img src="{{ asset('images/blogs/'.$blog->featured_image) }}" class="img-fluid rounded shadow-sm mb-4" alt="{{ $blog->title }}" style="width: 100%;">
+        @endif
+
+        <div class="blog-single-content">
+            {!! $blog->body !!}
+        </div>
+
+        <hr class="my-5">
+
+        {{-- Action Bar: Likes, Views, Share --}}
+        <div class="d-flex flex-wrap justify-content-between align-items-center mb-4">
+            <div>
+                <a href="javascript:void(0)" class="btn btn-light rounded-pill px-4 me-2" onclick="likeBlog({{ $blog->id }})">
+                    <i class="far fa-heart" id="like_icon"></i> <span id="like_span">{{ $blog->likes }} Like(s)</span>
+                </a>
+                <span class="text-muted ms-3"><i class="fas fa-eye"></i> {{ $blog->views }} Views</span>
+            </div>
+            <div class="social-share-links mt-3 mt-md-0">
+                <span class="me-3 font-weight-bold">Share:</span>
+                <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlcurrent() }}" class="btn btn-facebook" onclick="window.open(this.href,'newwindow', 'width=500,height=400'); return false;"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://twitter.com/intent/tweet?url={{ urlcurrent() }}" class="btn btn-twitter" onclick="window.open(this.href,'newwindow', 'width=500,height=400'); return false;"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.linkedin.com/shareArticle?mini=true&url={{ urlcurrent() }}" class="btn btn-linkedin" onclick="window.open(this.href,'newwindow', 'width=500,height=400'); return false;"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+        </div>
+
+        {{-- Author Section --}}
+        <div class="author-box shadow-sm">
+            <img src="{{ $blog->user->image ? asset('images/users/'.$blog->user->image) : asset('images/default-user.png') }}" class="author-img" alt="Author">
+            <div>
+                <h5 class="mb-1 font-weight-bold">{{ $blog->user->name }}</h5>
+                <p class="text-muted mb-0" style="font-size: 14px;">{{ $blog->user->designation ?? 'Expert Content Creator' }}</p>
+                <a href="{{ route('blogger.profile', $blog->user->id) }}" class="small text-primary">View Profile »</a>
+            </div>
+        </div>
+
+        {{-- Facebook Comments --}}
+        <div class="mt-5">
+            <h4 class="font-weight-bold mb-4"><i class="far fa-comments"></i> আলোচনা ও মন্তব্য</h4>
+            <div class="fb-comments" data-href="{{ Request::url() }}" data-width="100%" data-numposts="5"></div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     {{-- facebook comment plugin --}}
     <div id="fb-root"></div>
     <script>(function(d, s, id) {

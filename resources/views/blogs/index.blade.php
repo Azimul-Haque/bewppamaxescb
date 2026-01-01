c22d570e7b56752aaf3ca065a5c6479bebb7e481
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-02 05:18:49 +0600

    Commit Fri 01/02/2026  5:18:49.28 4091

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -100,7 +100,7 @@
         <div class="container">
             <div class="row">
                 @foreach ($blogs as $blog)
-                <div class="col-md-6 col-sm-12">
+                <div class="col-md-6 col-sm-12" style="padding-bottom: 20px;">
                     <div class="blog-card wow fadeIn">
                         <div class="blog-image-wrapper">
                             <a href="{{ route('blog.single', $blog->slug) }}">

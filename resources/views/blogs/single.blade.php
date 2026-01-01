3f713e9247ae749068d064ffc0a05e5e88a6a96b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-02 05:51:07 +0600

    Commit Fri 01/02/2026  5:51:07.32 13789

--- resources/views/blogs/single.blade.php
+++ resources/views/blogs/single.blade.php
@@ -100,8 +100,8 @@
         }
         .social-share-links .btn {
             border-radius: 50%;
-            width: 60px;
-            height: 60px;
+            width: 50px;
+            height: 50px;
             display: inline-flex;
             align-items: center;
             justify-content: center;

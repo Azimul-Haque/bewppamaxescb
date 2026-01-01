e3507df04eee906bd866cd7dfe3fbf4bb42b393b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-02 04:32:14 +0600

    Commit Fri 01/02/2026  4:32:14.31 21736

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -346,7 +346,7 @@
 
     //========= glightbox
     GLightbox({
-      'href': 'https://www.youtube.com/watch?v=r44RKWyfcFw&fbclid=IwAR21beSJORalzmzokxDRcGfkZA1AtRTE__l5N4r09HcGS5Y6vOluyouM9EM',
+      'href': 'https://www.youtube.com/watch?v=S1K1XRFOeNs',
       'type': 'video',
       'source': 'youtube', //vimeo, youtube or local
       'width': 900,

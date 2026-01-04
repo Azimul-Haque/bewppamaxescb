bd5afdce6d828b7d72c511a0562b226af9a88714
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-05 04:04:20 +0600

    Commit Mon 01/05/2026  4:04:20.22 10102

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -533,6 +533,7 @@ class ExamController extends Controller
                 $exam->negativepercentage    = $collection['negativepercentage'];
                 $exam->price_type = 1;
                 $exam->available_from    = Carbon::parse($collection['available_from']);
+                $exam->available_from    = Carbon::parse($collection['available_from']);
                 $exam->answer     = $collection['answer'];
                 $exam->save();
 

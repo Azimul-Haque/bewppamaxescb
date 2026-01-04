48930aabe3ec393b64146b64035513e329bfe52d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-05 04:04:47 +0600

    Commit Mon 01/05/2026  4:04:47.26 23088

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -535,6 +535,7 @@ class ExamController extends Controller
                 $exam->available_from    = Carbon::parse($collection['available_from']);
                 $exam->available_from    = Carbon::parse($collection['available_from']);
                 $exam->syllabus     = $collection['syllabus'];
+                // $exam->participation     = 0;
                 $exam->participation     = 0;
                 $exam->save();
 

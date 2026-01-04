20fa58cbdd68b5ad8f88288f7ecc98337adcf078
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-05 01:48:38 +0600

    Commit Mon 01/05/2026  1:48:38.27 22447

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -416,7 +416,7 @@
                     <div class="p-2 border rounded bg-white d-flex justify-content-between align-items-center shadow-sm">
                         <div style="max-width: 60%;">
                             <span class="d-block text-sm font-weight-bold text-dark">${name}</span>
-                            <div class="d-flex flex-wrap mt-1">
+                            <div class="">
                                 <span class="badge badge-success mr-1" style="font-size: 11px;">নিজস্ব প্রশ্ন: ${item.direct_q}</span>
                                 <span class="badge badge-secondary" style="font-size: 11px;">মোট (সবসহ): ${item.total_q}</span>
                             </div>

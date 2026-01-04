2f71e19db9fec30481db3d142c02a842786ab898
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-05 00:09:02 +0600

    Commit Mon 01/05/2026  0:09:02.28 15163

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -350,6 +350,8 @@
                         <button type="submit" class="btn btn-success">দাখিল করুন</button>
                     </div>
                 </form> --}}
+
+                
             </div>
         </div>
     </div>

1bdcccc26353ee351fcf6c4aa26494873f9f2679
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-05 05:53:50 +0600

    Commit Mon 01/05/2026  5:53:50.22 25954

--- resources/views/dashboard/courses/addexams.blade.php
+++ resources/views/dashboard/courses/addexams.blade.php
@@ -110,7 +110,7 @@
             <div class="modal-content">
                 <div class="modal-header bg-success">
                     <h5 class="modal-title" id="addExamModalLabel">
-                        প্রশ্ন হালনাগাদ
+                        প্রশ্নপত্র হালনাগাদ
                         <span id="examupdatingnumber"></span>
                     </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">

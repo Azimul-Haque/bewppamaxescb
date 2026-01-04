936f55ed5deec93427db1d56f0ca9c58d0c9090e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-05 01:55:04 +0600

    Commit Mon 01/05/2026  1:55:04.29 17367

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -259,7 +259,7 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form method="post" id="addautoquestionform" action="{{ route('dashboard.exams.question.auto.subs') }}">
+                <form method="post" id="addautoquestionform" action="{{ route('dashboard.exams.question.auto.fromsubs') }}">
                     <div class="modal-body" style="max-height: 70vh; overflow-y: auto;">
                         @csrf
                         <input type="hidden" name="exam_id" value="{{ $exam->id }}">

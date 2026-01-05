a3e636a27b44bd8191878d10fd3d8041bcdf10b8
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-05 06:28:43 +0600

    Commit Mon 01/05/2026  6:28:43.22 31099

--- resources/views/dashboard/courses/addexams.blade.php
+++ resources/views/dashboard/courses/addexams.blade.php
@@ -73,9 +73,11 @@
                 <div class="card-tools">
                     <input type="text" id="quickSearch" class="form-control form-control-sm" placeholder="নাম লিখে খুঁজুন...">
                 </div>
-                <div class="icheck-primary d-inline">
-                    <input type="checkbox" id="checkAll">
-                    <label for="checkAll">সবগুলো সিলেক্ট করুন</label>
+                <div class="mb-2">
+                    <div class="icheck-primary d-inline">
+                        <input type="checkbox" id="checkAll">
+                        <label for="checkAll">সবগুলো সিলেক্ট করুন</label>
+                    </div>
                 </div>
             </div>
             
@@ -84,9 +86,7 @@
                 <input type="hidden" name="course_id" value="{{ $course->id }}">
                 
                 <div class="card-body">
-                    <div class="mb-2">
-                        
-                    </div>
+                    
                     
                     <div class="row" id="examContainer">
                         @foreach($exams as $index => $exam)

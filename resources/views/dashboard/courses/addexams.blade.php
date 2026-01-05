a17e9ce43d4ccf9fccabcf0f89c3919e077ee82e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-05 06:51:16 +0600

    Commit Mon 01/05/2026  6:51:16.21 10004

--- resources/views/dashboard/courses/addexams.blade.php
+++ resources/views/dashboard/courses/addexams.blade.php
@@ -128,7 +128,7 @@
                             </div>
                         </div>
                         <div class="card-footer">
-                            <button class="btn btn-warning float-right shadow">সব মুছে ফেলুন</button>
+                            <button class="btn btn-warning shadow">সব মুছে ফেলুন</button>
                             <button type="submit" class="btn btn-primary float-right shadow">নির্বাচিত পরীক্ষাসমূহ সেভ করুন</button>
                             <div class="float-left">
                                 {{ $exams->links() }}

7e09c24c0ba4c9bdd3c05783a49ed2d4148efd6e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-05 06:30:14 +0600

    Commit Mon 01/05/2026  6:30:14.28 4787

--- resources/views/dashboard/courses/addexams.blade.php
+++ resources/views/dashboard/courses/addexams.blade.php
@@ -70,12 +70,12 @@
         <div class="card card-outline card-info">
             <div class="card-header">
                 <h3 class="card-title">পরীক্ষা নির্বাচন করুন</h3>
+                <div class="icheck-primary d-inline">
+                    <input type="checkbox" id="checkAll">
+                    <label for="checkAll">সবগুলো সিলেক্ট করুন</label>
+                </div>
                 <div class="card-tools">
                     <input type="text" id="quickSearch" class="form-control form-control-sm" placeholder="নাম লিখে খুঁজুন...">
-                    <div class="icheck-primary d-inline">
-                        <input type="checkbox" id="checkAll">
-                        <label for="checkAll">সবগুলো সিলেক্ট করুন</label>
-                    </div>
                 </div>
 
                

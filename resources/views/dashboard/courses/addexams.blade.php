dd4535eb6771edc560e90b5a3dc34e311cc65484
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-05 06:53:06 +0600

    Commit Mon 01/05/2026  6:53:06.32 6667

--- resources/views/dashboard/courses/addexams.blade.php
+++ resources/views/dashboard/courses/addexams.blade.php
@@ -130,7 +130,7 @@
                         <div class="card-footer">
                             <div class="float-right">
 
-                                <button class="btn btn-warning shadow" data-toggle="modal" data-target="#addTopicModal">সব মুছে ফেলুন</button>
+                                <button class="btn btn-warning shadow" data-toggle="modal" data-target="#deleteAllModal">সব মুছে ফেলুন</button>
                                 <button type="submit" class="btn btn-primary shadow">নির্বাচিত পরীক্ষাসমূহ সেভ করুন</button>
                             </div>
                             <div class="float-left">
@@ -142,7 +142,7 @@
             </div>
         </div>
 
-        <div class="modal fade" id="addTopicModal" tabindex="-1" role="dialog" aria-labelledby="addTopicModalLabel" aria-hidden="true" data-backdrop="static">
+        <div class="modal fade" id="deleteAllModal" tabindex="-1" role="dialog" aria-labelledby="addTopicModalLabel" aria-hidden="true" data-backdrop="static">
             <div class="modal-dialog" role="document">
               <div class="modal-content">
                 <div class="modal-header bg-warning">

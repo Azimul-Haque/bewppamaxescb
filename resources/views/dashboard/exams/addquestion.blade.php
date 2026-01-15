61095f1c36a8709a7f881e542d7df585d6d58b96
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-16 04:26:07 +0600

    Commit Fri 01/16/2026  4:26:07.27 14916

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -261,7 +261,6 @@
                             সিলেবাস: {{ $exam->syllabus }}
                         </div>
                     </h5>
-                    
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>

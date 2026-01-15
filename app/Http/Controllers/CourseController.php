977fc4292244e6bcb4356e86fb6a9665f7cea564
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-16 01:41:02 +0600

    Commit Fri 01/16/2026  1:41:02.29 10543

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -201,7 +201,7 @@ class CourseController extends Controller
                 ->where('examcategory_id', $course->category)
                 // ->orderByRaw(DB::raw("CASE WHEN id IN (" . (empty($existingExamIds) ? '0' : implode(',', $existingExamIds)) . ") THEN 0 ELSE 1 END"))
                 ->orderBy('id', 'desc')
-                ->paginate(30); // প্রতি পেজে ৫০টি করে ডাটা
+                ->paginate(30); // প্রতি পেজে ৩০টি করে ডাটা
 
         return view('dashboard.courses.addexams')
                                     ->withCourse($course)

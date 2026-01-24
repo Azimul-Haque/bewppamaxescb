4b1e09647da55bb3f5a7a69e5408ff7d7d35911e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-24 20:51:31 +0600

    Commit Sat 01/24/2026 20:51:31.22 8854

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -108,6 +108,7 @@
   </style>
 
   <link rel="preload" href="/fonts/kalpurush.woff2" as="font" type="font/woff2" crossorigin>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   
   @yield('third_party_stylesheets')
 </head>

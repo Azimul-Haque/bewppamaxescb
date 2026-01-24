679c33e7128364c161f53ec10016de8f6c1fb6f6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-01-24 20:47:42 +0600

    Commit Sat 01/24/2026 20:47:42.25 10517

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -7,7 +7,7 @@
 
 @section('third_party_stylesheets')
     @yield('third_party_stylesheets-s')
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     
     <style type="text/css">

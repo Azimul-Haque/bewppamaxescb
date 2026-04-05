6a8d68f0d715c6ad372cce635f3c664620428da7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-06 01:08:07 +0600

    Commit Mon 04/06/2026  1:08:07.25 22837

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -102,7 +102,9 @@
                                     <span class="badge @if($user->role == 'admin') badge-success @elseif($user->role == 'ambassador') badge-warning @else badge-info @endif badge-pill px-3">
                                         {{ ucfirst($user->role) }}
                                     </span>
-                                    {{-- {{ $user->isAmbassador() }} --}}
+                                    @if($user->isAmbassador())
+
+                                    @endif
                                 </td>
 
                                 <td>

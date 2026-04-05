f4892c4dd9a59fa05dce5d4903a7c5ffcf85db80
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-06 01:26:57 +0600

    Commit Mon 04/06/2026  1:26:57.22 9384

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -125,7 +125,7 @@
 
                                         @if($user->isAmbassador())
                                           <button type="button" class="btn btn-outline-success btn-sm" data-toggle="modal" data-target="#editUserModal{{ $user->id }}" title="Edit">
-                                              <i class="fas fa-hand-holding-dollar"></i>
+                                              <i class="fas fa-hand-holding-usd"></i>
                                           </button>
                                         @endif
                                         

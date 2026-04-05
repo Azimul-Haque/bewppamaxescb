cd3be9a1b23d8d05ee9b94425cbd1f3ecd708617
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-06 01:03:53 +0600

    Commit Mon 04/06/2026  1:03:53.22 28405

--- app/Models/AmbassadorProfile.php
+++ app/Models/AmbassadorProfile.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class AmbassadorProfile extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'balance', 'total_earned'];
+
+    // এই প্রোফাইলটি কোন ইউজারের, তা জানার জন্য রিলেশন
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

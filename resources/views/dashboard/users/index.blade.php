3db97535bdee9cebb8821945e7c7f62afec423ed
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-06 01:32:56 +0600

    Commit Mon 04/06/2026  1:32:56.27 25587

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -292,6 +292,9 @@
                                                           <button type="submit" class="btn btn-success" onclick="return confirm('আপনি কি নিশ্চিত যে টাকাটি পাঠানো হয়েছে এবং রেকর্ডটি সেভ করতে চান?')">
                                                               <i class="fas fa-paper-plane"></i> পেমেন্ট কনফার্ম করুন
                                                           </button>
+                                                          <button type="button" class="btn btn-success" onclick="confirmPayout('{{ $user->id }}')">
+                                                              <i class="fas fa-paper-plane"></i> পেমেন্ট কনফার্ম করুন
+                                                          </button>
                                                       </div>
                                                   </form>
                                               </div>

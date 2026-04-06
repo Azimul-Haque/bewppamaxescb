5cfb9dc1108e1e6889d30823395657de7fd1dc7d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-07 02:19:19 +0600

    Commit Tue 04/07/2026  2:19:19.31 26004

--- app/Models/PayoutRequest.php
+++ app/Models/PayoutRequest.php
@@ -11,7 +11,7 @@ class PayoutRequest extends Model
 
     protected $fillable = [
         'user_id', 'amount', 'payment_method', 
-        'payment_number', 'transaction_id', 'status'
+        'payment_number', 'status'
     ];
 
     // রিলেশন: কোন ইউজার টাকা দাবি করেছেন?

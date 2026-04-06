c9c514cb84f457a9048c9e6b82268bf38f1b9f2d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-07 04:14:43 +0600

    Commit Tue 04/07/2026  4:14:43.26 26017

--- app/Http/Controllers/BkashController.php
+++ app/Http/Controllers/BkashController.php
@@ -49,12 +49,12 @@ class BkashController extends Controller
         return view('bkash.final-payment')->withAmount($request->amount);
     }
 
-    public function prodPayment($amount, $mobile, $package_id, $ = null) {
+    public function prodPayment($amount, $mobile, $package_id, $referralcode = null) {
         return view('bkash.final-payment')
                             ->withAmount($amount)
                             ->withMobile($mobile)
                             ->withPackageid($package_id)
-                            ->withReferralcode($referral_code);
+                            ->withReferralcode($referralcode);
     }
 
     public function getToken(Request $request)

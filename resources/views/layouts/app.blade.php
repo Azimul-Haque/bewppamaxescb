ab6b72c87f337765b801266fcc3aa71a3c33ed70
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-07 01:43:32 +0600

    Commit Tue 04/07/2026  1:43:32.29 13582

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
               <div class="dropdown-divider"></div>
               <a href="{{ route('dashboard.messages') }}" class="dropdown-item">
                 <i class="fas fa-envelope mr-2"></i> 
-                {{ $unresolvedmessagecount }} new messages
+                {{ $unresolvedmessagecount }} টি নতুন মেসেজ
                 <span class="float-right text-muted text-sm">3 mins</span>
               </a>
               <div class="dropdown-divider"></div>

40f5cbc49a2b2bb8c6c3cb6790be4cdfd844f6c4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-15 00:12:26 +0600

    Commit Wed 04/15/2026  0:12:26.25 4205

--- resources/views/blogs/single.blade.php
+++ resources/views/blogs/single.blade.php
@@ -197,7 +197,7 @@
             <h4 class="font-weight-bold mb-4"><i class="far fa-comments"></i> আলোচনা ও মন্তব্য</h4>
             <div class="fb-comments" data-href="{{ Request::url() }}" data-width="100%" data-numposts="5"></div>
         </div> --}}
-        <div class="mt-5">
+        <div class="mt-5" style="height: 100%;">
             <div id="cusdis_thread"
               data-host="https://cusdis.com"
               data-app-id="00554008-2c15-466a-ba58-b6a114797e49"

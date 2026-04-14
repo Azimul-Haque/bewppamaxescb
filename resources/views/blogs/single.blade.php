545bfe58228e476e27e5eeb9aa79f4d83b8b4644
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-14 23:53:38 +0600

    Commit Tue 04/14/2026 23:53:38.29 18329

--- resources/views/blogs/single.blade.php
+++ resources/views/blogs/single.blade.php
@@ -204,6 +204,7 @@
 @section('third_party_scripts-s')
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script type="text/javascript">
+        document.querySelector('.fb-comments').setAttribute("data-href", window.location.href);
         $(document).ready(function(){
             $('.note-video-clip').each(function() {
                 var tmp = $(this).parent().html();

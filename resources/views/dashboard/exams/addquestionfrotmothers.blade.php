d249f7c35a388c999c0bd99b2ed74642d80b7e0a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-22 20:06:41 +0600

    Commit Wed 04/22/2026 20:06:41.30 2642

--- resources/views/dashboard/exams/addquestionfrotmothers.blade.php
+++ resources/views/dashboard/exams/addquestionfrotmothers.blade.php
@@ -89,7 +89,7 @@
             <div class="col-md-2">
                 <div class="card">
                     <div class="card-header">
-                      <h3 class="card-title">প্রশ্নের হিসাব</h3>
+                      <h3 class="card-title">সিলেবাস</h3>
           
                       <div class="card-tools">
                       </div>

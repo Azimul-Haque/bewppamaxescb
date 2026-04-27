e937781c0db9fb2aa43ce7cd14f876589d98482d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-27 22:37:06 +0600

    Commit Mon 04/27/2026 22:37:06.22 17582

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -331,6 +331,7 @@ class IndexController extends Controller
         // // Artisan::call('key:generate'); // not good for server and api or api token
         // Artisan::call('config:clear');
         Artisan::call('optimize:clear');
+        Artisan::call('sitemap:generate');
 
         DB::table('sessions')
                 ->where('last_activity', '<', Carbon::now()->subMinutes(config('session.lifetime'))->getTimestamp())

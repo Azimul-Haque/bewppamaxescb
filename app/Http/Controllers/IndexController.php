3df8d0f86f3b6586d7f39d373bb3241c2d530be6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-27 20:49:43 +0600

    Commit Mon 04/27/2026 20:49:43.23 26463

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -51,7 +51,7 @@ class IndexController extends Controller
         
         $packages = Package::where('status', 1)->get();
         $blogs = Blog::orderBy('id', 'desc')->get()->take(3);
-        $courses = Course::where()->orderBy('id', 'desc')->get()->take(5);
+        $courses = Course::where('status', 1)->orderBy('priority', 'asc')->get()->take(5);
 
         return view('index.index')
                     ->withPackages($packages)

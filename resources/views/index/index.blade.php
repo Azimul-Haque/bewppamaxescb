8b4758c9d0968973e00608675588fa8ba9d4ce08
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-04-27 19:34:25 +0600

    Commit Mon 04/27/2026 19:34:25.29 1498

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -434,41 +434,18 @@
                 "@context": "https://schema.org/",
                 "@type": "Product",
                 "name": "{{ $package->name }}",
-                // "image": [
-                //   "https://example.com/photos/1x1/photo.jpg",
-                //   "https://example.com/photos/4x3/photo.jpg",
-                //   "https://example.com/photos/16x9/photo.jpg"
-                //  ],
                 "description": "{{ $package->tagline }}",
-                "sku": "0446310786",
-                "mpn": "925872",
-                "brand": {
-                  "@type": "Brand",
-                  "name": "ACME"
-                },
-                "review": {
-                  "@type": "Review",
-                  "reviewRating": {
-                    "@type": "Rating",
-                    "ratingValue": 4,
-                    "bestRating": 5
-                  },
-                  "author": {
-                    "@type": "Person",
-                    "name": "Fred Benson"
-                  }
-                },
-                "aggregateRating": {
-                  "@type": "AggregateRating",
-                  "ratingValue": 4.4,
-                  "reviewCount": 89
-                },
                 "offers": {
-                  "@type": "AggregateOffer",
-                  "offerCount": 5,
-                  "lowPrice": 119.99,
-                  "highPrice": 199.99,
-                  "priceCurrency": "USD"
+                  "@type": "Offer",
+                  "url": "https://www.example.com/trinket_offer",
+                  "price": 10.00,
+                  "priceCurrency": "GBP",
+                  "priceSpecification": {
+                    "@type": "UnitPriceSpecification",
+                    "priceType": "https://schema.org/StrikethroughPrice",
+                    "price": 15.00,
+                    "priceCurrency": "GBP"
+                  }
                 }
               }
             </script>
